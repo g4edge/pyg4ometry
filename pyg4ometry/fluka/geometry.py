893b34db5ca21db4841e8d119eff07b7f96e6bf3
swalker92 <zwap082@live.rhul.ac.uk>
2018-04-23 18:42:56 +0100

    Added InfiniteEllipticalCylinder base class for XEC/YEC/ZEC.
    Useful for checking whether a solid is infinite

--- pyg4ometry/fluka/geometry.py
+++ pyg4ometry/fluka/geometry.py
@@ -290,6 +290,9 @@ class InfiniteCylinder(Body):
                                  self._scale * 0.5,
                                  0.0, 2*pi)
 
+class InfiniteEllipticalCylinder(Body):
+    """Currently just for type checking XEC, YEC, and ZEC.  No functionality."""
+    pass
 
 class InfiniteHalfSpace(Body):
     def __init__(self, name, parameters, translation=None, transformation=None):
