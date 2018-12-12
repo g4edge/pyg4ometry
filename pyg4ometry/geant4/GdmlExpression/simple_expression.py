826c2be0fae309ab73207c36b4d0a8aebbcaa0a6
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2018-12-12 18:45:17 +0000

    Handling of multiple operands for mult/div and add/sub evaluation. Placeholders for variable retrieval from a dict and function name interrogation.

--- pyg4ometry/geant4/GdmlExpression/simple_expression.py
+++ pyg4ometry/geant4/GdmlExpression/simple_expression.py
@@ -10,6 +10,7 @@ from GdmlExpressionVisitor import GdmlExpressionVisitor
 import math
 
 from IPython import embed
+import traceback
 
 class EvalVisitor(GdmlExpressionVisitor):
     def __init__(self):
@@ -19,6 +20,11 @@ class EvalVisitor(GdmlExpressionVisitor):
         name = ctx.ID().getText();
         value = self.visit(ctx.expr())
         self.memory[name] = value
+        return value
+
+    def visitVariable(self, ctx):
+        name = ctx.VARIABLE().getText();
+        value = self.defines.get(name, "1.5")
         return value
 
     def visitPrintExpr(self, ctx):
@@ -35,59 +41,86 @@ class EvalVisitor(GdmlExpressionVisitor):
     def visitInt(self, ctx):
         return int(ctx.INT().getText())
 
+    def visitScientific(self, ctx):
+        return float(ctx.SCIENTIFIC_NUMBER().getText())
+
     def visitMultiplyingExpression(self, ctx):
         left = float(self.visit(ctx.powExpression(0)))
-        right = float(self.visit(ctx.powExpression(1)))
-        if ctx.op.type == GdmlExpressionParser.TIMES:
-            return left * right
-        return left / right
+        # Perform all defined multiplications/divisions
+        # TODO: Super dumb and slow way of getting the order, rewrite
+        ops = [char for char in ctx.getText() if char=="*" or char=="/"]
+        for i in range(len(ctx.powExpression())-1):
+            print i
+            right = float(self.visit(ctx.powExpression(i+1)))
+            if ops[i] == "*":
+                left  *= right
+            else:
+                left  /= right
+        return left
 
     def visitExpression(self, ctx):
         left = float(self.visit(ctx.multiplyingExpression(0)))
-        right = float(self.visit(ctx.multiplyingExpression(1)))
-        if ctx.op.type == GdmlExpressionParser.PLUS:
-            return left + right
-        return left - right
+
+        # Perform all defined addtions/subtractions
+        # TODO: Super dumb and slow way of getting the order, rewrite
+        ops = [char for char in ctx.getText() if char=="+" or char=="-"]
+        for i in range(len(ctx.multiplyingExpression())-1):
+            right = float(self.visit(ctx.multiplyingExpression(i+1)))
+            if ops[i] == "+":
+                left += right
+            else:
+                left -= right
+        print left
+        return left
 
     def visitPowExpression(self, ctx):
-        left = float(self.visit(ctx.signedAtom(0)))
-        right = float(self.visit(ctx.signedAtom(1)))
-        return left ** right
+        base = float(self.visit(ctx.signedAtom(0)))
+        for i in range(len(ctx.POW())):
+            power = float(self.visit(ctx.signedAtom(i+1)))
+            base = base ** power
+
+        return base
 
     def visitParens(self, ctx):
         return self.visit(ctx.expression())
 
     def visitSignedAtom(self, ctx):
         sign = -1 if ctx.MINUS() else 1
+        value = 0
         if ctx.func():
             value = self.visit(ctx.func())
         elif ctx.atom():
             value = self.visit(ctx.atom())
-        elif self.signedAtom():
+        elif ctx.signedAtom():
             value = self.visit(ctx.signedAtom())
-        else:
-            raise SystemExit("Signed atom done fucked up.")
+        #else:
+        #    raise SystemExit("Invalid signed atom.") ##DEBUG####
 
         return sign*float(value)
 
     def visitAtom(self, ctx):
-        sign = -1 if ctx.MINUS() else 1
-        if ctx.func():
-            value = self.visit(ctx.func())
-        elif ctx.atom():
-            value = self.visit(ctx.atom())
-        elif self.signedAtom():
-            value = self.visit(ctx.signedAtom())
+        if ctx.constant():
+            value =  getattr(math, ctx.constant())
+        elif ctx.variable():
+            value = self.visit(ctx.variable())
+        elif ctx.expression(): # This handles expr with and without parens
+            value = self.visit(ctx.expression())
+        elif ctx.scientific():
+            value = self.visit(ctx.scientific())
         else:
-            raise SystemExit("Signed atom done fucked up.")
+            raise SystemExit("Invalid atom.") ##DEBUG####
 
-        return sign*float(value)
+        return float(value)
 
-    def visitFunCall(self, ctx):
-        function_name = ctx.ID().getText()
+    def visitFunc(self, ctx):
+        function_name = str(self.visit(ctx.funcname()))
         function = getattr(math, function_name)
-        arguments = [self.visit(expr) for expr in ctx.expr()]
+        arguments = [self.visit(expr) for expr in ctx.expression()]
         return function(*arguments)
+
+    def visitFuncname(self, ctx):
+        #TODO: fix this
+        return "cos"
 
 def main(argv):
     # create a CharStream that reads from standard input
@@ -96,6 +129,8 @@ def main(argv):
     else:
         input_str = sys.stdin.read()
         istream = InputStream(input_str)
+
+    print "Input expression: ", str(istream)
 
     # tokenise character stream
     lexer = GdmlExpressionLexer(istream)
