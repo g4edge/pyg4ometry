81c8a538f429b9b5122dbb2eaf2fd02abb792e57
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2018-12-14 20:16:07 +0000

    Use Expression for all internal workings of classes for defines, include Expression evaluation via float casting

--- pyg4ometry/geant4/GdmlDefines.py
+++ pyg4ometry/geant4/GdmlDefines.py
@@ -5,80 +5,92 @@ from ..geant4 import Expression as _Expression
 class Constant :
     def __init__(self, name, value, registry = None) :
         self.name  = name
-        self.value = value
+        self.expr = _Expression(value, registry=registry)
 
         registry.addDefine(self)
-            
+
+    def __float__(self) :
+        return self.expr.eval()
+    
     def __repr__(self) :
-        return 'Constant : '+self.name+' '+str(self.value)
+        return "Constant : {} = {}".format(self.name, str(self.expr))
 
 class Quantity :
     def __init__(self, name, value, unit, type, registry = None) :
         self.name  = name
-        self.expr  = _Expression(value,0)
+        self.expr  = _Expression(value, registry=registry)
         self.unit  = unit
         self.type  = type
 
         registry.addDefine(self)
-                        
+
+    def __float__(self) :
+        return self.expr.eval()
+
     def __repr__(self) :
-        return 'Quantity : '+self.name+' '+str(self.value)+' '+self.unit+' '+self.type
+        return "Quantity: {} = {} [{}] {}".format(self.name, str(self.expr), self.unit, self.type)
 
 class Variable :
     def __init__(self, name, value, registry = None) :
         self.name  = name
-        self.expr  = _Expression(value,0)
+        self.expr  = _Expression(value, registry=registry)
 
         registry.addDefine(self)
 
+    def __float__(self) :
+        return self.expr.eval()
+
     def __repr__(self) :
-        return 'Variable : '+self.name+' '+str(self.value)
+        return "Variable: {} = {}".format(self.name, str(self.expr))
 
 class Expression : 
     def __init__(self, name, value, registry = None) : 
         self.name = name
-        self.value = _Expression(value,0)
+        self.expr = _Expression(value, registry=registry)
 
         _registry.addDefine(self)
 
+    def __float__(self) :
+        return self.expr.eval()
+
     def __repr__(self) :
-        return 'Expression : '+self.name+' '+str(self.value)
+        return "Expression : {} = {}".format(self.expr, self.value)
 
 class Position :
     def __init__(self,name,x,y,z, registry = None) :
         self.name = name
-        self.x = _Expression(x,0)
-        self.y = _Expression(y,0)
-        self.z = _Expression(z,0)
+        self.x = _Expression(x, registry=registry)
+        self.y = _Expression(y, registry=registry)
+        self.z = _Expression(z, registry=registry)
         
         registry.addDefine(self)
 
     def __repr__(self) :
-        return 'Position : '+self.name+' '+str(self.x)+' '+str(self.y)+' '+str(self.z)
+        return "Position : {} = [{} {} {}]".format(self.name, str(self.x), str(self.y), str(self.z))
         
 class Rotation : 
     def __init__(self,name,rx,ry,rz, registry = None) :
         self.name = name
-        self.rx = _Expression(rx,0)
-        self.ry = _Expression(ry,0)
-        self.rz = _Expression(rz,0)
+        self.rx = _Expression(rx, registry=registry)
+        self.ry = _Expression(ry, registry=registry)
+        self.rz = _Expression(rz, registry=registry)
 
         registry.addDefine(self)
 
     def __repr__(self) :
-        return 'Rotation : '+self.name+' '+str(self.rx)+' '+str(self.ry)+' '+str(self.rz)
+        return "Rotation : {} = [{} {} {}]".format(self.name, str(self.rx), str(self.ry), str(self.rz))
 
 class Scale : 
     def __init__(self,name,sx,sy,sz, registry = None) :
         self.name = name
-        self.sx = _Expression(sx,0)
-        self.sy = _Expression(sy,0)
-        self.sz = _Expression(sz,0)
+        self.sx = _Expression(sx, registry=registry)
+        self.sy = _Expression(sy, registry=registry)
+        self.sz = _Expression(sz, registry=registry)
 
         registry.addDefine(self)        
 
     def __repr__(self) :
-        return 'Scale : '+self.name+' '+str(self.sx)+' '+str(self.sy)+' '+str(self.sz)
+        return "Scale : {} = [{} {} {}]".format(self.name, str(self.sx), str(self.sy), str(self.sz))
 
 class Matrix :
     def __init__(self,name, coldim, values, registry = None) :
@@ -91,4 +103,4 @@ class Matrix :
         registry.addDefine(self)
 
     def __repr__(self) :
-        return 'Matrix : '+self.name+' '+str(self.coldim)+' '+str(self.values)
+        return "Matrix : {} = {} {}".format(self.name, str(self.coldim), str(self.values))
