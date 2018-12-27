ce44815435bf355fb19cef8f0c2f662e36ba91e5
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2018-12-27 08:21:09 -0800

    Other neglected files

---
+++ pyg4ometry/gui/example1.py
@@ -0,0 +1,50 @@
+import sys
+import vtk
+from PyQt5 import QtCore, QtGui
+from PyQt5 import Qt
+
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+class MainWindow(Qt.QMainWindow):
+
+    def __init__(self, parent = None):
+        Qt.QMainWindow.__init__(self, parent)
+
+        self.frame = Qt.QFrame()
+        self.vl = Qt.QVBoxLayout()
+        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
+        self.vl.addWidget(self.vtkWidget)
+
+        self.ren = vtk.vtkRenderer()
+        self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
+
+        # Create source
+        source = vtk.vtkSphereSource()
+        source.SetCenter(0, 0, 0)
+        source.SetRadius(5.0)
+
+        # Create a mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(source.GetOutputPort())
+
+        # Create an actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+
+        self.ren.AddActor(actor)
+
+        self.ren.ResetCamera()
+
+        self.frame.setLayout(self.vl)
+        self.setCentralWidget(self.frame)
+
+        self.show()
+        self.iren.Initialize()
+        self.iren.Start()
+
+
+if __name__ == "__main__":
+    app = Qt.QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
