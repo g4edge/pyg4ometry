fa6c01e70aea527cf5c7ca61edfb2519cbf88137
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-02-03 20:38:50 +0000

    Test torus

--- pyg4ometry/geant4/solid/Torus.py
+++ pyg4ometry/geant4/solid/Torus.py
@@ -37,26 +37,24 @@ class Torus(_SolidBase):
 
 
     def pycsgmesh(self):
-#        if self.mesh :
-#            return self.mesh
 
-        self.basicmesh()
-        self.csgmesh()
+        pRmin = float(self.pRmin)
+        pRmax = float(self.pRmax)
+        pRtor = float(self.pRtor)
+        pSPhi = float(self.pSPhi)
+        pDPhi = float(self.pDPhi)
 
-        return self.mesh
-
-    def basicmesh(self):
         polygons = []
 
-        dTheta  = 2*_np.pi/self.nstack
-        dPhi    = 2*_np.pi/self.nslice
-        sphi    = self.pSPhi
-        stacks  = self.nstack
-        slices  = self.nslice
+        nstack  = self.nstack
+        nslice  = self.nslice
+        dTheta  = 2*_np.pi/nstack
+        dPhi    = 2*_np.pi/nslice
+
 
         def appendVertex(vertices, theta, phi, r):
             c = _Vector([0,0,0])
-            x = r*_np.cos(theta)+self.pRtor
+            x = r*_np.cos(theta)+pRtor
             z = r*_np.sin(theta)
             y = 0
             x_rot = _np.cos(phi)*x - _np.sin(phi)*y
@@ -69,58 +67,52 @@ class Torus(_SolidBase):
 
             vertices.append(_Vertex(c.plus(d), None))
 
-        rinout    = [self.pRmin, self.pRmax]
-        self.meshinout = []
+        rinout    = [pRmin, pRmax]
+        meshinout = []
 
         for r in rinout:
-            for j0 in range(slices):
+            for j0 in range(nslice):
                 j1 = j0 + 0.5
                 j2 = j0 + 1
-                for i0 in range(stacks):
+                for i0 in range(nstack):
                     i1 = i0 + 0.5
                     i2 = i0 + 1
                     verticesN = []
-                    appendVertex(verticesN, i1 * dTheta, j1 * dPhi + sphi, r)
-                    appendVertex(verticesN, i2 * dTheta, j2 * dPhi + sphi, r)
-                    appendVertex(verticesN, i0 * dTheta, j2 * dPhi + sphi, r)
+                    appendVertex(verticesN, i1 * dTheta, j1 * dPhi + pSPhi, r)
+                    appendVertex(verticesN, i2 * dTheta, j2 * dPhi + pSPhi, r)
+                    appendVertex(verticesN, i0 * dTheta, j2 * dPhi + pSPhi, r)
                     polygons.append(_Polygon(verticesN))
                     verticesS = []
-                    appendVertex(verticesS, i1 * dTheta, j1 * dPhi + sphi, r)
-                    appendVertex(verticesS, i0 * dTheta, j0 * dPhi + sphi, r)
-                    appendVertex(verticesS, i2 * dTheta, j0 * dPhi + sphi, r)
+                    appendVertex(verticesS, i1 * dTheta, j1 * dPhi + pSPhi, r)
+                    appendVertex(verticesS, i0 * dTheta, j0 * dPhi + pSPhi, r)
+                    appendVertex(verticesS, i2 * dTheta, j0 * dPhi + pSPhi, r)
                     polygons.append(_Polygon(verticesS))
                     verticesW = []
-                    appendVertex(verticesW, i1 * dTheta, j1 * dPhi + sphi, r)
-                    appendVertex(verticesW, i0 * dTheta, j2 * dPhi + sphi, r)
-                    appendVertex(verticesW, i0 * dTheta, j0 * dPhi + sphi, r)
+                    appendVertex(verticesW, i1 * dTheta, j1 * dPhi + pSPhi, r)
+                    appendVertex(verticesW, i0 * dTheta, j2 * dPhi + pSPhi, r)
+                    appendVertex(verticesW, i0 * dTheta, j0 * dPhi + pSPhi, r)
                     polygons.append(_Polygon(verticesW))
                     verticesE = []
-                    appendVertex(verticesE, i1 * dTheta, j1 * dPhi + sphi, r)
-                    appendVertex(verticesE, i2 * dTheta, j0 * dPhi + sphi, r)
-                    appendVertex(verticesE, i2 * dTheta, j2 * dPhi + sphi, r)
+                    appendVertex(verticesE, i1 * dTheta, j1 * dPhi + pSPhi, r)
+                    appendVertex(verticesE, i2 * dTheta, j0 * dPhi + pSPhi, r)
+                    appendVertex(verticesE, i2 * dTheta, j2 * dPhi + pSPhi, r)
                     polygons.append(_Polygon(verticesE))
 
             mesh      = _CSG.fromPolygons(polygons)
-            self.meshinout.append(mesh)
+            meshinout.append(mesh)
             polygons = []
 
-
-        self.mesh = self.meshinout[1]
-        return self.mesh
-
-
-    def csgmesh(self):
-        if self.pRmin != 0:
-            self.mesh  = self.meshinout[0].subtract(self.meshinout[1])
+        if pRmin != 0:
+            mesh  = meshinout[0].subtract(meshinout[1])
 
         else:
-           self.mesh = self.meshinout[1].inverse()
+           mesh = meshinout[1].inverse()
 
-        if self.pDPhi != 2*_np.pi:
-            wrmax    = 3*self.pRtor #make sure intersection wedge is much larger than solid
-            wzlength = 5*self.pRmax
+        if pDPhi != 2*_np.pi:
+            wrmax    = 3*pRtor #make sure intersection wedge is much larger than solid
+            wzlength = 5*pRmax
 
-            pWedge = _Wedge("wedge_temp",wrmax, self.pSPhi, self.pDPhi, wzlength).pycsgmesh()
-            self.mesh = pWedge.intersect(self.mesh)
+            pWedge = _Wedge("wedge_temp",wrmax, pSPhi, pDPhi, wzlength).pycsgmesh()
+            mesh = pWedge.intersect(mesh)
 
-        return self.mesh
+        return mesh
