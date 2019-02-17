db2c88ec88ccd714f721e4de1eea4924201c2443
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-02-17 17:33:06 +0000

    Tidy and remove old imports

--- pyg4ometry/visualisation/__init__.py
+++ pyg4ometry/visualisation/__init__.py
@@ -1,6 +1,8 @@
+from Mesh                 import Mesh
+from VisualisationOptions import VisualisationOptions
+from VtkViewer            import *
+
 # from Viewer import viewLogicalVolume, viewWorld, Viewer
-from Mesh import Mesh
-from VtkViewer import *
 #from Viewer import Viewer
 #from Convert import *
 #from Writer import *
