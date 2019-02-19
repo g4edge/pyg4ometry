419b3436f7c053432a92b1106f0b7dc3ededfa39
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-02-19 15:33:33 +0000

    Remove debug and lists for position and roation

--- pyg4ometry/geant4/PhysicalVolume.py
+++ pyg4ometry/geant4/PhysicalVolume.py
@@ -8,10 +8,20 @@ class PhysicalVolume(object):
     '''Geant4 Physical volume class'''
 
     def __init__(self, rotation, position, logicalVolume, name,
-                 motherVolume, scale=[1,1,1], debug=False, registry=None):
+                 motherVolume, scale=[1,1,1], registry=None):
         super(PhysicalVolume, self).__init__()
 
         # need to determine type or rotation and position, as should be Position or Rotation type
+        from pyg4ometry.gdml import Defines as _Defines
+
+        print position
+        print rotation
+        print registry
+
+        if isinstance(position,list) :             
+            position = _Defines.Position(name+"_pos",position[0],position[1],position[2],registry,False)
+        if isinstance(rotation,list) :
+            rotation = _Defines.Rotation(name+"_rot",rotation[1],rotation[1],rotation[2],registry,False)
 
         # geant4 required objects
         self.rotation      = rotation
@@ -21,7 +31,6 @@ class PhysicalVolume(object):
         self.motherVolume  = motherVolume
         self.motherVolume.add(self)
         self.scale         = scale
-        self.debug         = debug
         
         # physical visualisation options 
         self.visOptions    = _VisOptions()
