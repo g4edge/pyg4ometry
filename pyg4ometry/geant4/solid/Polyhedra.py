8aae11f49ff9c3376376d610203d33cceca1c830
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2019-03-13 18:27:50 +0100

    Standardise Polyhedra solid

--- pyg4ometry/geant4/solid/Polyhedra.py
+++ pyg4ometry/geant4/solid/Polyhedra.py
@@ -2,11 +2,11 @@ from SolidBase import SolidBase as _SolidBase
 from ..Registry import registry as _registry
 from Polycone import Polycone as _Polycone
 import numpy as _np
-
+import logging as _log
 
 class Polyhedra(_SolidBase):
     def __init__(self, name, phiStart, phiTotal, numSide, numZPlanes,
-                 zPlane, rInner, rOuter, register=True):
+                 zPlane, rInner, rOuter, registry=True):
         """
         Constructs a polyhedra.
 
@@ -30,21 +30,39 @@ class Polyhedra(_SolidBase):
         self.zPlane     = zPlane
         self.rInner     = rInner
         self.rOuter     = rOuter
-        self.mesh = None
-        if register:
-            _registry.addSolid(self)
+
+        self.dependents = []
+
+        if registry:
+            registry.addSolid(self)
+
+    def __repr__(self):
+        return "Polyhedra : {} {} {} {} {}".format(self.name, self.phiStart,
+                                                  self.phiTotal, self.numSide,
+                                                  self.numZPlanes)
 
     def pycsgmesh(self):
-        self.basicmesh()
-        return self.mesh
+        mesh = self.basicmesh()
+        return mesh
 
     def basicmesh(self):
-        fillfrac  = self.phiTotal/(2*_np.pi)
-        slices    = (self.numSide)/fillfrac
+        _log.info("polyhedra.antlr>")
 
-        ph        = _Polycone(self.name, self.phiStart, self.phiTotal,
-                              self.zPlane, self.rInner, self.rOuter,
-                              nslice=int(_np.ceil(slices)), register=False)
-        self.mesh = ph.pycsgmesh()
+        phiStart = float(self.phiStart)
+        phiTotal = float(self.phiTotal)
+        numSide = int(self.numSide)
+        numZPlanes = int(self.numZPlanes)
+        zPlane = [float(val) for val in self.zPlane]
+        rInner = [float(val) for val in self.rInner]
+        rOuter = [float(val) for val in self.rOuter]
 
-        return self.mesh
+        _log.info("polyhedra.pycsgmesh>")
+        fillfrac  = phiTotal/(2*_np.pi)
+        slices    = (numSide)/fillfrac
+
+        ph        = _Polycone(self.name, phiStart, phiTotal,
+                              zPlane, rInner, rOuter,
+                              nslice=int(_np.ceil(slices)), registry=None)
+        mesh = ph.pycsgmesh()
+
+        return mesh
