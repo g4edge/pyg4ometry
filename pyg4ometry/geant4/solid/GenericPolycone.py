da4d6b6fd6cb6c46a79a5659ed418d53fa4ee39d
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2019-03-13 18:31:55 +0100

    Standardise GenericPolycone solid

--- pyg4ometry/geant4/solid/GenericPolycone.py
+++ pyg4ometry/geant4/solid/GenericPolycone.py
@@ -1,6 +1,5 @@
 from SolidBase import SolidBase as _SolidBase
 from Wedge import Wedge as _Wedge
-from ..Registry import registry as _registry
 from ...pycsg.core import CSG as _CSG
 from ...pycsg.geom import Vector as _Vector
 from ...pycsg.geom import Vertex as _Vertex
@@ -12,7 +11,7 @@ from copy import deepcopy as _dc
 
 class GenericPolycone(_SolidBase):
     def __init__(self, name, pSPhi, pDPhi, pNSide, pNRZ, pR, pZ,
-                 nslice=16, register=True):
+                 nslice=16, registry=None):
         """
         Constructs a solid of rotation using an arbitrary 2D surface defined by a series of (r,z) coordinates.
 
@@ -38,17 +37,25 @@ class GenericPolycone(_SolidBase):
         self.dependents = []
 
         if register:
-            if register:
-                _registry.addSolid(self)
+            _registry.addSolid(self)
 
     def pycsgmesh(self):
+        _log.info("polycone.antlr>")
+        pSPhi = float(self.pSPhi)
+        pDPhi = float(self.pDPhi)
+        pNSide = int(self.pNSide)
+        pNRZ = int(self.pNRZ)
+        pR = [float(val) for val in self.pR]
+        pZ = [float(val) for val in self.pZ]
+
+        _log.info("polycone.pycsgmesh>")
         polygons = []
         polygonsT = []
         polygonsB = []
 
-        anglerange=self.pDPhi-self.pSPhi
+        anglerange=pDPhi-pSPhi
         dPhi  = 2*_np.pi/self.nslice
-        stacks  = self.pNRZ
+        stacks  = pNRZ
         slices  = self.nslice
 
         def defineVertex(theta, z, r):
@@ -62,7 +69,7 @@ class GenericPolycone(_SolidBase):
                 z)
             return d
         def GetAngle(i):
-            return self.pSPhi+(anglerange/slices)*(i)
+            return pSPhi+(anglerange/slices)*(i)
 
         def appendVertex(vertices, theta, z, r, norm=[]):
             c = _Vector([0,0,0])
@@ -82,24 +89,24 @@ class GenericPolycone(_SolidBase):
 
 		#Bottom
         verticesB = []
-        for i in range(self.pNRZ):
-            appendVertex(verticesB,self.pSPhi,self.pZ[i],self.pR[i])
+        for i in range(pNRZ):
+            appendVertex(verticesB,pSPhi,pZ[i],pR[i])
         polygons.append(_Polygon(verticesB))
 
 	    #Top
         verticesT = []
-        for i in range(self.pNRZ):
-            appendVertex(verticesT,self.pDPhi,self.pZ[i],self.pR[i])
+        for i in range(pNRZ):
+            appendVertex(verticesT,pDPhi,pZ[i],pR[i])
 
 		#Midsection
         for l in range(1, slices+1):
-            maxn = self.pNRZ
+            maxn = pNRZ
             for n in range(maxn):
                 n_up = (n+1)%maxn
-                polygons.append(_Polygon([_Vertex(defineVertex(GetAngle(l), self.pZ[n], self.pR[n]), None),
-                                          _Vertex(defineVertex(GetAngle(l), self.pZ[n_up], self.pR[n_up]), None),
-                                          _Vertex(defineVertex(GetAngle(l-1), self.pZ[n_up], self.pR[n_up]), None),
-                                          _Vertex(defineVertex(GetAngle(l-1), self.pZ[n], self.pR[n]), None)]))
+                polygons.append(_Polygon([_Vertex(defineVertex(GetAngle(l), pZ[n], pR[n]), None),
+                                          _Vertex(defineVertex(GetAngle(l), pZ[n_up], pR[n_up]), None),
+                                          _Vertex(defineVertex(GetAngle(l-1), pZ[n_up], pR[n_up]), None),
+                                          _Vertex(defineVertex(GetAngle(l-1), pZ[n], pR[n]), None)]))
         polygons.append(_Polygon(verticesT))
-        self.mesh     = _CSG.fromPolygons(polygons)
-        return self.mesh
+        mesh     = _CSG.fromPolygons(polygons)
+        return mesh
