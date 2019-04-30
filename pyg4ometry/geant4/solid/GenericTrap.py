62c410febda3a6ebe4ca4982b50234d4ad050da5
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2019-04-30 17:47:47 +0100

    Introduced user-controlled layering to Arbitrary Trapezoid that enables smoother meshing

--- pyg4ometry/geant4/solid/GenericTrap.py
+++ pyg4ometry/geant4/solid/GenericTrap.py
@@ -1,19 +1,16 @@
 from SolidBase import SolidBase as _SolidBase
-from Wedge import Wedge as _Wedge
-from Plane import Plane as _Plane
 from ...pycsg.core import CSG as _CSG
 from ...pycsg.geom import Vector as _Vector
 from ...pycsg.geom import Vertex as _Vertex
 from ...pycsg.geom import Polygon as _Polygon
 
-from copy import deepcopy as _dc
 import logging as _log
 
 import numpy as _np
 
 class GenericTrap(_SolidBase):
     def __init__(self, name, v1x, v1y, v2x, v2y, v3x, v3y, v4x, v4y,
-                 v5x, v5y, v6x, v6y, v7x, v7y, v8x, v8y, dz, registry=None):
+                 v5x, v5y, v6x, v6y, v7x, v7y, v8x, v8y, dz, registry=None, nstack=20):
         """
         Constructs an arbitrary trapezoid using two quadrilaterals sitting
         on two parallel planes. Vertices 1-4 define the quadrilateral at -dz and
@@ -43,6 +40,7 @@ class GenericTrap(_SolidBase):
         self.type = 'GenericTrap'
         self.name = name
         self.dz = dz
+        self.nstack = nstack
 
         vars_in = locals()
         for i in range(1, 9):
@@ -73,6 +71,34 @@ class GenericTrap(_SolidBase):
                   sign_z*float(self.dz))
         return vertex
 
+    def makeLayers(self, verts_bot, verts_top):
+
+        layers = []
+
+        z1 = 2*float(self.dz)
+        z0 = -float(self.dz)
+
+        for i in range(self.nstack+1):
+            z = z0 + i*z1/self.nstack
+            dz = (z - z0) / (z1 - z0)
+
+            verts_i = []
+            for k in range(4):
+                v_bot = verts_bot[k]
+                v_top = verts_top[k]
+
+                # Linearly interpolate
+                x = v_bot[0] + (dz * (v_top[0] - v_bot[0]))
+                y = v_bot[1] + (dz * (v_top[1] - v_bot[1]))
+
+                verts_i.append(_Vertex(_Vector(x, y, z), None))
+
+            layers.append(verts_i)
+
+        return layers
+
+
+
     def pycsgmesh(self):
         _log.info('arb8.pycsgmesh> antlr')
 
@@ -86,20 +112,33 @@ class GenericTrap(_SolidBase):
                 verts_top.append(vert)
 
         # Correct ordering enures correct surface normals
-        if self.polygon_area(verts_top) < 0:
+        if self.polygon_area(verts_top) > 0:
             verts_top = list(reversed(verts_top))
 
-        if self.polygon_area(verts_bot) < 0:
+        if self.polygon_area(verts_bot) > 0:
             verts_bot = list(reversed(verts_bot))
 
-        vt = [_Vertex(_Vector(v), None) for v in verts_top]
-        vb = [_Vertex(_Vector(v), None) for v in verts_bot]
+        all_verts = self.makeLayers(verts_bot, verts_top)
 
         _log.info('arb8.pycsgmesh> mesh')
-        mesh  = _CSG.fromPolygons([_Polygon([vt[0], vt[1], vt[2], vt[3]]),
-                                   _Polygon([vb[3], vb[2], vb[1], vb[0]]),
-                                   _Polygon([vb[0], vb[1], vt[1], vt[0]]),
-                                   _Polygon([vb[1], vb[2], vt[2], vt[1]]),
-                                   _Polygon([vb[2], vb[3], vt[3], vt[2]]),
-                                   _Polygon([vb[3], vb[0], vt[0], vt[3]])])
+        polygons = []
+
+        # Mesh top and bottom pieces
+        polygons.append(_Polygon([all_verts[0][3], all_verts[0][2],
+                                  all_verts[0][1], all_verts[0][0]])) # Bot
+
+        polygons.append(_Polygon([all_verts[-1][0], all_verts[-1][1],
+                                  all_verts[-1][2], all_verts[-1][3]])) # Top
+
+        # Mesh the sides
+        for i0 in range(len(all_verts)-1):
+            i1 = i0 + 1
+            vts_l = all_verts[i0]
+            vts_u = all_verts[i1]
+
+            for k0 in range(4): # 4 vertexes
+                k1 = (k0+1) % 4
+                polygons.append(_Polygon([vts_l[k0], vts_l[k1], vts_u[k1], vts_u[k0]]))
+
+        mesh  = _CSG.fromPolygons(polygons)
         return mesh
