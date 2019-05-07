3e6e2976dd0e2fa96014471a7ca98a639a48e2c3
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2019-05-07 19:00:08 +0100

    Use a dictionary for unit lookup - faster and cleaner

--- pyg4ometry/gdml/Units.py
+++ pyg4ometry/gdml/Units.py
@@ -1,43 +1,21 @@
 import numpy as _np
 
-nm = 1.0e-6
-um = 1.0e-3
-mm = 1.0
-m  = 1.0e3
-km = 1.0e6
+units = {
+    # Length units
+    "nm" : 1.0e-6,
+    "um" : 1.0e-3,
+    "mm" : 1.0,
+    "cm" : 1.0e1,
+    "m"  : 1.0e3,
+    "km" : 1.0e6,
 
-deg = _np.pi/180.0
-rad = 1.0
+    # Angle units
+    "deg" : _np.pi/180.0,
+    "rad" : 1.0,
 
-def lunit(unitString) : 
-    if unitString == "nm" :
-        return nm
-    elif unitString == "um" : 
-        return um 
-    elif unitString == "mm" : 
-        return mm
-    elif unitString == "m" : 
-        return m
-    elif unitString == "km" :
-        return km
-    else : 
-        return None
+    # Other
+    "none" : 1.0,
+    }
 
-def aunit(unitString) : 
-    if unitString == "deg" : 
-        return deg
-    elif unitString == "rad" : 
-        return rad
-    else :
-        return None 
-
-def unit(unitString) : 
-    lu = lunit(unitString)
-    au = aunit(unitString)
-    if lu : 
-        return lu
-    if au :
-        return au
-
-    if unitString == "none" : 
-        return 1.0
+def unit(unitString) :
+    return units.get(unitString, None)
