872e38b4273811dad3bd2835a3844bc04760f7f6
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2019-08-20 11:46:29 +0100

    Add a dict for storing body transforms.

--- pyg4ometry/flukaNew/FlukaRegistry.py
+++ pyg4ometry/flukaNew/FlukaRegistry.py
@@ -2,11 +2,12 @@ from collections import OrderedDict as _OrderedDict
 
 class FlukaRegistry:
     def __init__(self) :
-        self.bodyDict     = _OrderedDict()
-        self.regionDict   = _OrderedDict()
-        self.materialDict = _OrderedDict()
-        self.latticeDict  = _OrderedDict()
-        self.cardDict     = _OrderedDict()
+        self.bodyDict          = _OrderedDict()
+        self.bodyTransformDict = _OrderedDict()
+        self.regionDict        = _OrderedDict()
+        self.materialDict      = _OrderedDict()
+        self.latticeDict       = _OrderedDict()
+        self.cardDict          = _OrderedDict()
 
     def addBody(self, body):
         self.bodyDict.add(body)
