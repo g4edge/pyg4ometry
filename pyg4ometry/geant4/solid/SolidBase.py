c45a4f2285a332701ad65325f34c1efa312aee43
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-09-01 23:40:43 +0100

    . (period) is also allowed in file names

--- pyg4ometry/geant4/solid/SolidBase.py
+++ pyg4ometry/geant4/solid/SolidBase.py
@@ -9,7 +9,8 @@ class SolidBase(object):
         @name.setter
         def name(self, name):
 		non_alphanum = set([c for c in name if not c.isalnum()])
-		non_alphanum = non_alphanum.difference('_') # underscors are # OK.
+		non_alphanum = non_alphanum.difference('_') # underscores are # OK.
+		non_alphanum = non_alphanum.difference('.') #
 		if non_alphanum:
 			msg = "Forbidden character(s) in name \"{}\": {}.".format(
 				name, list(non_alphanum))
