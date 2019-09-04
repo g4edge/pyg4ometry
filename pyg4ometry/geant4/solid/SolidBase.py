fe8ac0e8f3c0ecfdd9e3cdc0fa2aa2f85a3e2265
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2019-09-04 13:14:16 +0100

    Added a method to the solid baseclass to evaliate parameters to floats.

--- pyg4ometry/geant4/solid/SolidBase.py
+++ pyg4ometry/geant4/solid/SolidBase.py
@@ -1,21 +1,31 @@
 class SolidBase(object):
-	def __init__(self,name = "noname") :
-		self._name = name
+    """
+    Base class for all solis
+    """
+    def __init__(self, name, registry):
+        self._name = name
+        self.registry = registry
 
-	@property
-	def name(self):
-		return self._name
-	
-        @name.setter
-        def name(self, name):
-		non_alphanum = set([c for c in name if not c.isalnum()])
-		non_alphanum = non_alphanum.difference('_') # underscores are # OK.
-		non_alphanum = non_alphanum.difference('.') #
-		if non_alphanum:
-			msg = "Forbidden character(s) in name \"{}\": {}.".format(
-				name, list(non_alphanum))
-			raise ValueError(msg)
-		if not name[0].isalpha():
-			msg = "First char of name \"{}\" must be a letter.".format(name)
-			raise ValueError(msg)
-		self._name = name
+    def evaluateParameter(self, obj):
+        from pyg4ometry.gdml.Defines import evaluateToFloat
+        return evaluateToFloat(self.registry, obj)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        non_alphanum = set([c for c in name if not c.isalnum()])
+        non_alphanum = non_alphanum.difference('_')  # underscores are # OK.
+        non_alphanum = non_alphanum.difference('.')  #
+        if non_alphanum:
+            msg = \
+                'Forbidden character(s) in name "{}": {}.'.format(name,
+                    list(non_alphanum))
+            raise ValueError(msg)
+        if not name[0].isalpha():
+            msg = \
+                'First char of name "{}" must be a letter.'.format(name)
+            raise ValueError(msg)
+        self._name = name
