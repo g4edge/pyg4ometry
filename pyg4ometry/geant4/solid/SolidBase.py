95ad91f7870da940a574a00db31b85cd75c24116
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2019-09-11 15:54:27 +0100

    Add methods to the solid base class to make properties at runtime. Those properties override the default access and assignment for attributes, enabling registering of changes in solid parameters. When a solid parameter is changed, the solid is earmarked for re-meshing in the visualiser.

--- pyg4ometry/geant4/solid/SolidBase.py
+++ pyg4ometry/geant4/solid/SolidBase.py
@@ -3,12 +3,37 @@ class SolidBase(object):
     Base class for all solis
     """
     def __init__(self, name="noname", registry=None):
-        self._name = name
+        self.name = name
         self.registry = registry
 
     def evaluateParameter(self, obj):
         from pyg4ometry.gdml.Defines import evaluateToFloat
         return evaluateToFloat(self.registry, obj)
+
+    def _addProperty(self, attribute):
+        #create local setter and getter with a particular attribute name
+        if hasattr(self.__class__, attribute):
+            return
+
+        getter = lambda self: self._getProperty(attribute)
+        setter = lambda self, value: self._setProperty(attribute, value)
+
+        #construct property attribute and add it to the class
+        setattr(self.__class__, attribute, property(fget=getter,
+                                                    fset=setter,
+                                                    doc="Auto-generated method"))
+
+    def _setProperty(self, attribute, value):
+        #print "Setting: %s = %s" %(attribute, value) # DEBUG
+        # When setting a parameter of a solid, add the solid name
+        # to a list of edited solids in the registry. This forces a fresh
+        # meshing for visualisation, instead of using the cached mesh.
+        self.registry.registerSolidEdit(self)
+        setattr(self, '_' + attribute, value)
+
+    def _getProperty(self, attribute):
+        #print "Getting: %s" %str(attribute) # DEBUG
+        return getattr(self, "_" + attribute)
 
     @property
     def name(self):
