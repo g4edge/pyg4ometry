ce2dd4e4c672dcf2ef2dda7ab671cd7602c1428d
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-10-29 08:41:21 +0000

    Non CSG meshing

--- pyg4ometry/geant4/solid/CutTubs.py
+++ pyg4ometry/geant4/solid/CutTubs.py
@@ -3,6 +3,11 @@ from SolidBase import SolidBase as _SolidBase
 from Tubs import Tubs as _Tubs
 from Plane import Plane as _Plane
 
+from ...pycsg.core import CSG as _CSG
+from ...pycsg.geom import Vertex as _Vertex
+from ...pycsg.geom import Vector as _Vector
+from ...pycsg.geom import Polygon as _Polygon
+
 import numpy as _np
 import logging as _log
 
@@ -54,8 +59,8 @@ class CutTubs(_SolidBase):
         # Low norm and high norm exlcluded as they are lists
         return "Cut tubs : {} {} {} {} {} {}".format(self.name, self.pRMin, self.pRMax,
                                                         self.pDz, self.pSPhi, self.pDPhi)
-    def pycsgmesh(self):
-
+    def pycsgmeshOld(self):
+        # 0.0381021499634 80
         _log.info('cuttubs.pycsgmesh> antlr')
 
         import pyg4ometry.gdml.Units as _Units #TODO move circular import
@@ -88,3 +93,129 @@ class CutTubs(_SolidBase):
             return mesh
         else : 
             return mesh
+
+
+    def pycsgmesh(self):
+        # 0.00943803787231 66
+        _log.info('tubs.pycsgmesh> antlr')
+
+        import pyg4ometry.gdml.Units as _Units #TODO move circular import
+        luval = _Units.unit(self.lunit)
+        auval = _Units.unit(self.aunit)
+
+        pRMin = self.evaluateParameter(self.pRMin)*luval
+        pSPhi = self.evaluateParameter(self.pSPhi)*auval
+        pDPhi = self.evaluateParameter(self.pDPhi)*auval # issue with 2*pi
+        pDz   = self.evaluateParameter(self.pDz)*luval/2
+        pRMax = self.evaluateParameter(self.pRMax)*luval
+
+        pHighNorm = [val*luval for val in self.evaluateParameter(self.pHighNorm)]
+        pLowNorm = [val*luval for val in self.evaluateParameter(self.pLowNorm)]
+
+        _log.info('tubs.pycsgmesh> mesh')
+
+        polygons = []
+
+        dPhi = pDPhi/self.nslice
+
+        for i in range(0,self.nslice,1) :
+
+            i1 = i
+            i2 = i+1
+
+            p1 = dPhi*i1 + pSPhi
+            p2 = dPhi*i2 + pSPhi
+
+            xRMinP1     = pRMin*_np.cos(p1)
+            yRMinP1     = pRMin*_np.sin(p1)
+            xRMaxP1     = pRMax*_np.cos(p1)
+            yRMaxP1     = pRMax*_np.sin(p1)
+
+            xRMinP2     = pRMin*_np.cos(p2)
+            yRMinP2     = pRMin*_np.sin(p2)
+            xRMaxP2     = pRMax*_np.cos(p2)
+            yRMaxP2     = pRMax*_np.sin(p2)
+
+            zLowRMinP1  = (-pLowNorm[2]*pDz - pLowNorm[0]*xRMinP1 - pLowNorm[1]*yRMinP1)/pLowNorm[2]
+            zLowRMaxP1  = (-pLowNorm[2]*pDz - pLowNorm[0]*xRMaxP1 - pLowNorm[1]*yRMaxP1)/pLowNorm[2]
+            zHighRMinP1 = (pHighNorm[2]*pDz - pHighNorm[0]*xRMinP1 - pHighNorm[1]*yRMinP1)/pHighNorm[2]
+            zHighRMaxP1 = (pHighNorm[2]*pDz - pHighNorm[0]*xRMaxP1 - pHighNorm[1]*yRMaxP1)/pHighNorm[2]
+
+            zLowRMinP2  = (-pLowNorm[2]*pDz - pLowNorm[0]*xRMinP2 - pLowNorm[1]*yRMinP2)/pLowNorm[2]
+            zLowRMaxP2  = (-pLowNorm[2]*pDz - pLowNorm[0]*xRMaxP2 - pLowNorm[1]*yRMaxP2)/pLowNorm[2]
+            zHighRMinP2 = (pHighNorm[2]*pDz - pHighNorm[0]*xRMinP2 - pHighNorm[1]*yRMinP2)/pHighNorm[2]
+            zHighRMaxP2 = (pHighNorm[2]*pDz - pHighNorm[0]*xRMaxP2 - pHighNorm[1]*yRMaxP2)/pHighNorm[2]
+
+            ###########################
+            # wedge ends
+            ###########################
+            if pDPhi != 2*_np.pi and i == 0:
+                vWedg = []
+                vWedg.append(_Vertex([xRMinP1,yRMinP1, zLowRMinP1],None))
+                vWedg.append(_Vertex([xRMinP1,yRMinP1, zHighRMinP1],None))
+                vWedg.append(_Vertex([xRMaxP1,yRMaxP1, zHighRMaxP1],None))
+                vWedg.append(_Vertex([xRMaxP1,yRMaxP1, zLowRMaxP1],None))
+                polygons.append(_Polygon(vWedg))
+
+            if pDPhi != 2*_np.pi and i == self.nslice-1 :
+                vWedg = []
+                vWedg.append(_Vertex([xRMinP2,yRMinP2, zLowRMinP2],None))
+                vWedg.append(_Vertex([xRMaxP2,yRMaxP2, zLowRMaxP2],None))
+                vWedg.append(_Vertex([xRMaxP2,yRMaxP2, zHighRMaxP2],None))
+                vWedg.append(_Vertex([xRMinP2,yRMinP2, zHighRMinP2],None))
+                polygons.append(_Polygon(vWedg))
+
+            ###########################
+            # tube ends
+            ###########################
+            if pRMin == 0:
+                vEnd = []
+                vEnd.append(_Vertex([0,0,-pDz],None))
+                vEnd.append(_Vertex([xRMaxP1, yRMaxP1, zLowRMaxP1],None))
+                vEnd.append(_Vertex([xRMaxP2, yRMaxP2, zLowRMaxP2],None))
+                polygons.append(_Polygon(vEnd))
+
+                vEnd = []
+                vEnd.append(_Vertex([ 0,0, pDz],None))
+                vEnd.append(_Vertex([xRMaxP2, yRMaxP2, zHighRMaxP2],None))
+                vEnd.append(_Vertex([xRMaxP1, yRMaxP1, zHighRMaxP1],None))
+                polygons.append(_Polygon(vEnd))
+
+            else :
+                vEnd = []
+                vEnd.append(_Vertex([xRMinP1, yRMinP1, zLowRMinP1],None))
+                vEnd.append(_Vertex([xRMaxP1, yRMaxP1, zLowRMaxP1],None))
+                vEnd.append(_Vertex([xRMaxP2, yRMaxP2, zLowRMaxP2],None))
+                vEnd.append(_Vertex([xRMinP2, yRMinP2, zLowRMinP2],None))
+
+                polygons.append(_Polygon(vEnd))
+
+                vEnd = []
+                vEnd.append(_Vertex([xRMinP1, yRMinP1, zHighRMinP1],None))
+                vEnd.append(_Vertex([xRMinP2, yRMinP2, zHighRMinP2],None))
+                vEnd.append(_Vertex([xRMaxP2, yRMaxP2, zHighRMaxP2],None))
+                vEnd.append(_Vertex([xRMaxP1, yRMaxP1, zHighRMaxP1],None))
+
+                polygons.append(_Polygon(vEnd))
+
+            ###########################
+            # Curved cylinder faces
+            ###########################
+            vCurv = []
+            vCurv.append(_Vertex([xRMaxP1, yRMaxP1, zLowRMaxP1],None))
+            vCurv.append(_Vertex([xRMaxP1, yRMaxP1, zHighRMaxP1],None))
+            vCurv.append(_Vertex([xRMaxP2, yRMaxP2, zHighRMaxP2],None))
+            vCurv.append(_Vertex([xRMaxP2, yRMaxP2, zLowRMaxP2],None))
+            polygons.append(_Polygon(vCurv))
+
+            if pRMin != 0 :
+                vCurv = []
+                vCurv.append(_Vertex([xRMinP1, yRMinP1, zLowRMinP1], None))
+                vCurv.append(_Vertex([xRMinP2, yRMinP2, zLowRMinP2], None))
+                vCurv.append(_Vertex([xRMinP2, yRMinP2, zHighRMinP2],None))
+                vCurv.append(_Vertex([xRMinP1, yRMinP1, zHighRMinP1],None))
+                polygons.append(_Polygon(vCurv))
+
+        mesh = _CSG.fromPolygons(polygons)
+
+        return mesh
