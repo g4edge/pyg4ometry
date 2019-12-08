034c10b289c659afc60f08fad056a9fdd9db3369
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-12-08 10:59:51 +0000

    First working PyQt5 GUI application

---
+++ pyg4ometry/gui/MainWindow.py
@@ -0,0 +1,102 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QCheckBox, QFileDialog, QLayout, QVBoxLayout, QPushButton
+from QVTKRenderWindowInteractor import  QVTKRenderWindowInteractor
+
+class MainWindow(QMainWindow):
+    
+    def __init__(self):
+        super(MainWindow,self).__init__()
+
+        self.initUI()
+
+        
+        
+    def initUI(self):
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        fileMenu = menubar.addMenu('File')
+
+        openAction = QAction('Open', self)
+        openAction.setShortcut('Ctrl+F')
+        openAction.setStatusTip('Open GDML file')
+        openAction.triggered.connect(self.openFileNameDialog)
+        fileMenu.addAction(openAction)
+
+        saveAction = QAction('Save', self)
+        saveAction.setShortcut('Ctrl+S')
+        saveAction.setStatusTip('Save GDML file')
+        saveAction.triggered.connect(self.saveFileDialog)
+        fileMenu.addAction(saveAction)
+
+        exitAction = QAction('Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+        fileMenu.addAction(exitAction)
+
+        fileMenu = menubar.addMenu('Actions')
+
+
+        from vtk.vtkFiltersSources import vtkConeSource
+        from vtk.vtkRenderingCore import vtkActor, vtkPolyDataMapper, vtkRenderer
+
+        vtkWidget  = QVTKRenderWindowInteractor()
+        self.setCentralWidget(vtkWidget)
+
+
+        vtkWidget.Initialize()
+        vtkWidget.Start()
+        # if you don't want the 'q' key to exit comment this.
+        vtkWidget.AddObserver("ExitEvent", lambda o, e, a=app: a.quit())
+
+        ren = vtkRenderer()
+        vtkWidget.GetRenderWindow().AddRenderer(ren)
+
+        cone = vtkConeSource()
+        cone.SetResolution(8)
+
+        coneMapper = vtkPolyDataMapper()
+        coneMapper.SetInputConnection(cone.GetOutputPort())
+
+        coneActor = vtkActor()
+        coneActor.SetMapper(coneMapper)
+
+        ren.AddActor(coneActor)
+
+        # show the widget
+        vtkWidget.show()
+
+
+
+
+
+
+
+
+
+        self.statusBar().showMessage('Ready')
+        self.setGeometry(300, 300, 1000, 750)
+        self.setWindowTitle('pyg4ometry')
+        self.show()
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+    
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            print(fileName)
+
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
