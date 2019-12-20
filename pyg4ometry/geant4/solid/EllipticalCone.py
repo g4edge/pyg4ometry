8deb0cf920f028a8f979a9a7f591b9b52254b922
Ben Shellswell <zdap036@live.rhul.ac.uk>
2019-12-20 14:33:08 +0000

    accelerated meshing

--- pyg4ometry/geant4/solid/EllipticalCone.py
+++ pyg4ometry/geant4/solid/EllipticalCone.py
@@ -54,35 +54,35 @@ class EllipticalCone(_SolidBase):
         if pzTopCut <= -zMax/2.0 or pzTopCut > zMax/2.0:
             raise ValueError("zMax must be greater than pzTopCut")
 
-    def pycsgmesh(self):
+    def pycsgmeshOld(self):
 
         _log.info("ellipticalcone.antlr>")
 
-        import pyg4ometry.gdml.Units as _Units #TODO move circular import 
+        import pyg4ometry.gdml.Units as _Units  # TODO move circular import
         luval = _Units.unit(self.lunit)
 
-        pxSemiAxis = self.evaluateParameter(self.pxSemiAxis)*luval
-        pySemiAxis = self.evaluateParameter(self.pySemiAxis)*luval
-        zMax       = self.evaluateParameter(self.zMax)*luval
-        pzTopCut   = self.evaluateParameter(self.pzTopCut)*luval
-        
+        pxSemiAxis = self.evaluateParameter(self.pxSemiAxis) * luval
+        pySemiAxis = self.evaluateParameter(self.pySemiAxis) * luval
+        zMax = self.evaluateParameter(self.zMax) * luval
+        pzTopCut = self.evaluateParameter(self.pzTopCut) * luval
+
         _log.info("ellipticalcone.pycsgmesh>")
         polygons = []
 
-        sz      = -zMax/2.
-        dz      = zMax/self.nstack
-        dTheta  = 2*_np.pi/self.nslice
-        stacks  = self.nstack
-        slices  = self.nslice
+        sz = -zMax / 2.
+        dz = zMax / self.nstack
+        dTheta = 2 * _np.pi / self.nslice
+        stacks = self.nstack
+        slices = self.nslice
 
         # semix and semiy are fractions of zmax - calculate absolute numbers
         dxabs = pxSemiAxis * zMax
         dyabs = pySemiAxis * zMax
 
         def appendVertex(vertices, theta, z, dx=dxabs, dy=dyabs, norm=[]):
-            c = _Vector([0,0,0])
-            x = dx*(((zMax - z)/zMax)*_np.cos(theta)) #generate points on an ellipse
-            y = dy*(((zMax - z)/zMax)*_np.sin(theta))
+            c = _Vector([0, 0, 0])
+            x = dx * (((zMax - z) / zMax) * _np.cos(theta))  # generate points on an ellipse
+            y = dy * (((zMax - z) / zMax) * _np.sin(theta))
             d = _Vector(
                 x,
                 y,
@@ -92,7 +92,6 @@ class EllipticalCone(_SolidBase):
             else:
                 n = _Vector(norm)
             vertices.append(_Vertex(c.plus(d), n))
-
 
         for j0 in range(slices):
             j1 = j0 + 0.5
@@ -126,17 +125,117 @@ class EllipticalCone(_SolidBase):
 
             vertices = []
 
-            appendVertex(vertices, i0 * dTheta, sz, norm=[0,0,1])
-            appendVertex(vertices, 0, sz, dx=0, dy=0, norm=[0,0,1])
-            appendVertex(vertices, i1 * dTheta, sz, norm=[0,0,1])
+            appendVertex(vertices, i0 * dTheta, sz, norm=[0, 0, 1])
+            appendVertex(vertices, 0, sz, dx=0, dy=0, norm=[0, 0, 1])
+            appendVertex(vertices, i1 * dTheta, sz, norm=[0, 0, 1])
             polygons.append(_Polygon(vertices))
 
             vertices = []
-            appendVertex(vertices, i1 * dTheta, stacks * dz + sz, norm=[0,0,-1])
-            appendVertex(vertices, 0, slices*dz + sz, dx=0, dy=0, norm=[0,0,-1])
-            appendVertex(vertices, i0 * dTheta, stacks * dz + sz, norm=[0,0,-1])
+            appendVertex(vertices, i1 * dTheta, stacks * dz + sz, norm=[0, 0, -1])
+            appendVertex(vertices, 0, slices * dz + sz, dx=0, dy=0, norm=[0, 0, -1])
+            appendVertex(vertices, i0 * dTheta, stacks * dz + sz, norm=[0, 0, -1])
             polygons.append(_Polygon(vertices))
 
-        mesh  = _CSG.fromPolygons(polygons)
+        mesh = _CSG.fromPolygons(polygons)
+
+        return mesh
+
+    def pycsgmesh(self):
+        _log.info("ellipticalcone.antlr>")
+
+        import pyg4ometry.gdml.Units as _Units  # TODO move circular import
+        luval = _Units.unit(self.lunit)
+
+        pxSemiAxis = self.evaluateParameter(self.pxSemiAxis) * luval
+        pySemiAxis = self.evaluateParameter(self.pySemiAxis) * luval
+        zMax = self.evaluateParameter(self.zMax) * luval
+        pzTopCut = self.evaluateParameter(self.pzTopCut) * luval
+
+        _log.info("ellipticalcone.pycsgmesh>")
+        polygons = []
+
+        # length of complete cone
+        L = zMax/pzTopCut
+
+        dTheta = 2 * _np.pi / self.nslice
+        slices = self.nslice
+
+        # semix and semiy are fractions of zmax - calculate absolute numbers
+        dx = pxSemiAxis * zMax
+        dy = pySemiAxis * zMax
+
+        for i0 in range(0,slices):
+
+            i1 = i0
+            i2 = i0 + 1
+
+            z1 = zMax/2
+            x1 = dx * ((L-z1 - zMax/2)/L) * _np.cos(dTheta*i1)
+            y1 = dy * ((L-z1 - zMax/2)/L) * _np.sin(dTheta*i1)
+
+            z2 = -zMax/2
+            x2 = dx * ((L-z2 - zMax/2)/L) * _np.cos(dTheta*i1)
+            y2 = dy * ((L-z2 - zMax/2)/L) * _np.sin(dTheta*i1)
+
+            z3 = -zMax/2
+            x3 = dx * ((L-z3 - zMax/2)/L) * _np.cos(dTheta*i2)
+            y3 = dy * ((L-z3 - zMax/2)/L) * _np.sin(dTheta*i2)
+
+            z4 = zMax/2
+            x4 = dx * ((L-z4 - zMax/2)/L) * _np.cos(dTheta*i2)
+            y4 = dy * ((L-z4 - zMax/2)/L) * _np.sin(dTheta*i2)
+
+            vertices = []
+
+            vertices.append(_Vertex([x1,y1,z1], None))
+            vertices.append(_Vertex([x2,y2,z2], None))
+            vertices.append(_Vertex([x3,y3,z3], None))
+            vertices.append(_Vertex([x4,y4,z4], None))
+
+            polygons.append(_Polygon(vertices))
+
+            # bottom
+            verticesb = []
+
+            z1b = -zMax/2
+            x1b = dx * ((L - z1b - zMax / 2) / L) * _np.cos(dTheta * i1)
+            y1b = dy * ((L - z1b - zMax / 2) / L) * _np.sin(dTheta * i1)
+
+            z2b = -zMax/2
+            x2b = 0
+            y2b = 0
+
+            z3b = -zMax/2
+            x3b = dx * ((L - z3b - zMax / 2) / L) * _np.cos(dTheta * i2)
+            y3b = dy * ((L - z3b - zMax / 2) / L) * _np.sin(dTheta * i2)
+
+            verticesb.append(_Vertex([x1b, y1b, z1b], None))
+            verticesb.append(_Vertex([x2b, y2b, z2b], None))
+            verticesb.append(_Vertex([x3b, y3b, z3b], None))
+
+            polygons.append(_Polygon(verticesb))
+
+            #top
+            verticest = []
+
+            z1t = zMax/2
+            x1t = dx * ((L - z1t - zMax / 2) / L) * _np.cos(dTheta * i1)
+            y1t = dy * ((L - z1t - zMax / 2) / L) * _np.sin(dTheta * i1)
+
+            z2t = zMax/2
+            x2t = 0
+            y2t = 0
+
+            z3t = zMax/2
+            x3t = dx * ((L - z3t - zMax / 2) / L) * _np.cos(dTheta * i2)
+            y3t = dy * ((L - z3t - zMax / 2) / L) * _np.sin(dTheta * i2)
+
+            verticest.append(_Vertex([x1t, y1t, z1t], None))
+            verticest.append(_Vertex([x2t, y2t, z2t], None))
+            verticest.append(_Vertex([x3t, y3t, z3t], None))
+
+            polygons.append(_Polygon(verticest))
+
+        mesh = _CSG.fromPolygons(polygons)
 
         return mesh
