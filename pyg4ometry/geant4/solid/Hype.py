f3aa5b79937b0d479d28cf7062c70b3da8a77bab
Ben Shellswell <zdap036@live.rhul.ac.uk>
2019-12-20 14:04:20 +0000

    accerlerated meshing

--- pyg4ometry/geant4/solid/Hype.py
+++ pyg4ometry/geant4/solid/Hype.py
@@ -73,7 +73,7 @@ class Hype(_SolidBase):
         if float(self.innerRadius) > float(self.outerRadius):
             raise ValueError("Inner radius must be less than outer radius.")
 
-    def pycsgmesh(self):
+    def pycsgmeshOld(self):
         _log.info('hype.pycsgmesh> antlr')
 
 
@@ -180,4 +180,149 @@ class Hype(_SolidBase):
         else:
             mesh = meshinout[0]
 
+        return mesh
+
+
+    def pycsgmesh(self):
+        _log.info('hype.pycsgmesh> antlr')
+
+
+        import pyg4ometry.gdml.Units as _Units #TODO move circular import
+        luval = _Units.unit(self.lunit)
+        auval = _Units.unit(self.aunit)
+
+        innerRadius = self.evaluateParameter(self.innerRadius)*luval
+        outerRadius = self.evaluateParameter(self.outerRadius)*luval
+        innerStereo = self.evaluateParameter(self.innerStereo)*auval
+        outerStereo = self.evaluateParameter(self.outerStereo)*auval
+        LenZ = self.evaluateParameter(self.lenZ)
+        halfLenZ    = self.evaluateParameter(self.lenZ)/2.0*luval
+
+        _log.info('hype.pycsgmesh> mesh')
+
+        dz     = LenZ/self.nstack
+        sz     = -halfLenZ
+        dTheta = 2*_np.pi/self.nslice
+        stacks = self.nstack
+        slices = self.nslice
+
+        polygons = []
+
+
+        for j0 in range(slices):
+
+            j1 = j0
+            j2 = j0 + 1
+
+            for i0 in range(stacks):
+
+                i1 = i0
+                i2 = i0 + 1
+
+                z1_outer = i1 * dz + sz
+                r1_outer = _np.sqrt(outerRadius**2 + (z1_outer*_np.tan(outerStereo)) ** 2)
+                x1_rot_outer = _np.cos(dTheta * j1) * r1_outer
+                y1_rot_outer = _np.sin(dTheta * j1) * r1_outer
+
+                z2_outer = i2 * dz + sz
+                r2_outer = _np.sqrt(outerRadius**2 + (z2_outer*_np.tan(outerStereo)) ** 2)
+                x2_rot_outer = _np.cos(dTheta * j1) * r2_outer
+                y2_rot_outer = _np.sin(dTheta * j1) * r2_outer
+
+                z3_outer = i2 * dz + sz
+                r3_outer = _np.sqrt(outerRadius**2 + (z3_outer*_np.tan(outerStereo)) ** 2)
+                x3_rot_outer = _np.cos(dTheta * j2) * r3_outer
+                y3_rot_outer = _np.sin(dTheta * j2) * r3_outer
+
+                z4_outer = i1 * dz + sz
+                r4_outer = _np.sqrt(outerRadius**2 + (z4_outer*_np.tan(outerStereo)) ** 2)
+                x4_rot_outer = _np.cos(dTheta * j2) * r4_outer
+                y4_rot_outer = _np.sin(dTheta * j2) * r4_outer
+
+
+                vertices_outer = []
+
+                vertices_outer.append(_Vertex([x1_rot_outer, y1_rot_outer, z1_outer], None))
+                vertices_outer.append(_Vertex([x2_rot_outer, y2_rot_outer, z2_outer], None))
+                vertices_outer.append(_Vertex([x3_rot_outer, y3_rot_outer, z3_outer], None))
+                vertices_outer.append(_Vertex([x4_rot_outer, y4_rot_outer, z4_outer], None))
+
+                polygons.append(_Polygon(vertices_outer))
+
+                if innerRadius != 0:
+
+                    z1_inner = i1 * dz + sz
+                    r1_inner = _np.sqrt(innerRadius**2 + (z1_inner*_np.tan(innerStereo)) ** 2)
+                    x1_rot_inner = _np.cos(dTheta * j1) * r1_inner
+                    y1_rot_inner = _np.sin(dTheta * j1) * r1_inner
+
+                    z2_inner = i2 * dz + sz
+                    r2_inner = _np.sqrt(innerRadius**2 + (z2_inner*_np.tan(innerStereo)) ** 2)
+                    x2_rot_inner = _np.cos(dTheta * j1) * r2_inner
+                    y2_rot_inner = _np.sin(dTheta * j1) * r2_inner
+
+                    z3_inner = i2 * dz + sz
+                    r3_inner = _np.sqrt(innerRadius**2 + (z3_inner*_np.tan(innerStereo)) ** 2)
+                    x3_rot_inner = _np.cos(dTheta * j2) * r3_inner
+                    y3_rot_inner = _np.sin(dTheta * j2) * r3_inner
+
+                    z4_inner = i1 * dz + sz
+                    r4_inner = _np.sqrt(innerRadius**2 + (z4_inner*_np.tan(innerStereo)) ** 2)
+                    x4_rot_inner = _np.cos(dTheta * j2) * r4_inner
+                    y4_rot_inner = _np.sin(dTheta * j2) * r4_inner
+
+                    vertices_inner = []
+
+                    vertices_inner.append(_Vertex([x1_rot_inner, y1_rot_inner, z1_inner], None))
+                    vertices_inner.append(_Vertex([x2_rot_inner, y2_rot_inner, z2_inner], None))
+                    vertices_inner.append(_Vertex([x3_rot_inner, y3_rot_inner, z3_inner], None))
+                    vertices_inner.append(_Vertex([x4_rot_inner, y4_rot_inner, z4_inner], None))
+
+                    polygons.append(_Polygon(vertices_inner))
+
+                    if i1 == 0:
+
+                        verticesTop = []
+
+                        verticesTop.append(_Vertex([x1_rot_inner, y1_rot_inner, z1_inner], None))
+                        verticesTop.append(_Vertex([x1_rot_outer, y1_rot_outer, z1_outer], None))
+                        verticesTop.append(_Vertex([x4_rot_outer, y4_rot_outer, z4_outer], None))
+                        verticesTop.append(_Vertex([x4_rot_inner, y4_rot_inner, z4_inner], None))
+
+                        polygons.append(_Polygon(verticesTop))
+
+                    if i2 == stacks:
+
+                        verticesBottom = []
+
+                        verticesBottom.append(_Vertex([x3_rot_inner, y3_rot_inner, z3_inner], None))
+                        verticesBottom.append(_Vertex([x3_rot_outer, y3_rot_outer, z3_outer], None))
+                        verticesBottom.append(_Vertex([x2_rot_outer, y2_rot_outer, z2_outer], None))
+                        verticesBottom.append(_Vertex([x2_rot_inner, y2_rot_inner, z2_inner], None))
+
+                        polygons.append(_Polygon(verticesBottom))
+
+                if innerRadius == 0:
+
+                    if i1 == 0:
+
+                        verticesTop = []
+
+                        verticesTop.append(_Vertex([x4_rot_outer, y4_rot_outer, z4_outer], None))
+                        verticesTop.append(_Vertex([0, 0, -halfLenZ], None))
+                        verticesTop.append(_Vertex([x1_rot_outer, y1_rot_outer, z1_outer], None))
+
+                        polygons.append(_Polygon(verticesTop))
+
+                    if i2 == stacks:
+
+                        verticesBottom = []
+
+                        verticesBottom.append(_Vertex([x2_rot_outer, y2_rot_outer, z2_outer], None))
+                        verticesBottom.append(_Vertex([0, 0, halfLenZ], None))
+                        verticesBottom.append(_Vertex([x3_rot_outer, y3_rot_outer, z3_outer], None))
+
+                    polygons.append(_Polygon(verticesBottom))
+
+        mesh = _CSG.fromPolygons(polygons)
         return mesh
