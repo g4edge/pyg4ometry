3773ef757e21a5554b183f6e3e45024695f9f7f8
Ben Shellswell <zdap036@live.rhul.ac.uk>
2019-12-20 14:51:57 +0000

    accelerated meshing

--- pyg4ometry/geant4/solid/Ellipsoid.py
+++ pyg4ometry/geant4/solid/Ellipsoid.py
@@ -65,7 +65,7 @@ class Ellipsoid(_SolidBase):
                                                       self.pySemiAxis, self.pzSemiAxis,
                                                       self.pzBottomCut, self.pzTopCut)
 
-    def pycsgmesh(self):
+    def pycsgmeshOld(self):
 
         _log.info('ellipsoid.pycsgmesh>')
         basicmesh = self.basicmesh()
@@ -152,5 +152,136 @@ class Ellipsoid(_SolidBase):
         pTopCut     = _Plane("pTopCut", topNorm, pzTopCut).pycsgmesh()
         pBottomCut  = _Plane("pBottomCut", botNorm, pzBottomCut).pycsgmesh()
         mesh   = basicmesh.subtract(pBottomCut).subtract(pTopCut)
+
+        return mesh
+
+    def pycsgmesh(self):
+        _log.info("ellipsoid.antlr>")
+
+        import pyg4ometry.gdml.Units as _Units  # TODO move circular import
+        luval = _Units.unit(self.lunit)
+
+        pxSemiAxis  = self.evaluateParameter(self.pxSemiAxis) * luval
+        pySemiAxis  = self.evaluateParameter(self.pySemiAxis) * luval
+        pzSemiAxis  = self.evaluateParameter(self.pzSemiAxis) * luval
+        pzBottomCut = float(self.pzBottomCut) * luval
+        pzTopCut    = float(self.pzTopCut)    * luval
+
+        _log.info("ellipsoid.pycsgmesh>")
+        polygons = []
+
+        slices = self.nslice
+        stacks = self.nstack
+
+        dTheta = 2 * _np.pi / self.nslice
+
+        PhiTop =          _np.arccos(pzTopCut   / pzSemiAxis)
+        PhiBot = _np.pi - _np.arccos(pzBottomCut/-pzSemiAxis)
+
+      #  PhiTop = 0.5
+      #   PhiBot = 3
+
+       # TopRadius = _np.srt( (pxSemiAxis * np_cos())**2 * (pySemiAxis * _np.sin())**2)
+
+        # yt = (pySemiAxis*_np.sqrt(1-(pzTopCut/pzSemiAxis)**2))
+        # xt = (pxSemiAxis*_np.sqrt(1-(pzTopCut/pzSemiAxis)**2))
+        # rt = _np.sqrt(xt**2 + yt**2)
+        #
+        # PhiTop = _np.arctan(rt/pzTopCut)
+
+
+
+
+        dPhi = (PhiBot - PhiTop) / stacks
+
+        # print('TOP = {}, BOT = {}, yt = {}, xt = {}, rt = {},{}'.format(PhiTop,PhiBot,yt,xt,rt,(pzTopCut/pzSemiAxis)**2))
+
+
+        for i0 in range(0,slices):
+            i1 = i0
+            i2 = i0 + 1
+
+            if PhiBot < _np.pi:
+
+                i1 = i0
+                i2 = i0 + 1
+
+                vertices = []
+
+               # PhiBot = -_np.arcsin(pzBottomCut/pzSemiAxis)
+
+                x1 = pxSemiAxis * _np.sin(PhiBot) * _np.cos(i1 * dTheta)
+                y1 = pySemiAxis * _np.sin(PhiBot) * _np.sin(i1 * dTheta)
+                z1 = pzBottomCut
+
+                x2 = 0
+                y2 = 0
+                z2 = pzBottomCut
+
+                x3 = pxSemiAxis * _np.sin(PhiBot) * _np.cos(i2 * dTheta)
+                y3 = pySemiAxis * _np.sin(PhiBot) * _np.sin(i2 * dTheta)
+                z3 = pzBottomCut
+
+                vertices.append(_Vertex([x1, y1, z1], None))
+                vertices.append(_Vertex([x2, y2, z2], None))
+                vertices.append(_Vertex([x3, y3, z3], None))
+
+                polygons.append(_Polygon(vertices))
+
+            if PhiTop > 0:
+
+                i1 = i0
+                i2 = i0 + 1
+
+                vertices = []
+
+                x1 = pxSemiAxis * _np.sin(PhiTop) * _np.cos(i1 * dTheta)
+                y1 = pySemiAxis * _np.sin(PhiTop) * _np.sin(i1 * dTheta)
+                z1 = pzTopCut
+
+                x2 = pxSemiAxis * _np.sin(PhiTop) * _np.cos(i2 * dTheta)
+                y2 = pySemiAxis * _np.sin(PhiTop) * _np.sin(i2 * dTheta)
+                z2 = pzTopCut
+
+                x3 = 0
+                y3 = 0
+                z3 = pzTopCut
+
+                vertices.append(_Vertex([x1, y1, z1], None))
+                vertices.append(_Vertex([x2, y2, z2], None))
+                vertices.append(_Vertex([x3, y3, z3], None))
+
+                polygons.append(_Polygon(vertices))
+
+            for j0 in range(stacks):
+                j1 = j0
+                j2 = j0 + 1
+
+                x1 = pxSemiAxis * _np.sin((j1 * dPhi) + PhiTop) * _np.cos(i1 * dTheta)
+                y1 = pySemiAxis * _np.sin((j1 * dPhi) + PhiTop) * _np.sin(i1 * dTheta)
+                z1 = pzSemiAxis * _np.cos((j1 * dPhi) + PhiTop)
+
+                x2 = pxSemiAxis * _np.sin((j2 * dPhi) + PhiTop) * _np.cos(i1 * dTheta)
+                y2 = pySemiAxis * _np.sin((j2 * dPhi) + PhiTop) * _np.sin(i1 * dTheta)
+                z2 = pzSemiAxis * _np.cos((j2 * dPhi) + PhiTop)
+
+                x3 = pxSemiAxis * _np.sin((j2 * dPhi) + PhiTop) * _np.cos(i2 * dTheta)
+                y3 = pySemiAxis * _np.sin((j2 * dPhi) + PhiTop) * _np.sin(i2 * dTheta)
+                z3 = pzSemiAxis * _np.cos((j2 * dPhi) + PhiTop)
+
+                x4 = pxSemiAxis * _np.sin((j1 * dPhi) + PhiTop) * _np.cos(i2 * dTheta)
+                y4 = pySemiAxis * _np.sin((j1 * dPhi) + PhiTop) * _np.sin(i2 * dTheta)
+                z4 = pzSemiAxis * _np.cos((j1 * dPhi) + PhiTop)
+
+                vertices = []
+
+                vertices.append(_Vertex([x1, y1, z1], None))
+                vertices.append(_Vertex([x2, y2, z2], None))
+                vertices.append(_Vertex([x3, y3, z3], None))
+                vertices.append(_Vertex([x4, y4, z4], None))
+
+                polygons.append(_Polygon(vertices))
+
+        mesh = _CSG.fromPolygons(polygons)
 
         return mesh
