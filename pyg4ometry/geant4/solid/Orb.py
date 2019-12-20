5f2616237c2481718fabc63bb1cd2209dca8c8f5
Ben Shellswell <zdap036@live.rhul.ac.uk>
2019-12-20 14:10:39 +0000

    accelerated meshing

--- pyg4ometry/geant4/solid/Orb.py
+++ pyg4ometry/geant4/solid/Orb.py
@@ -46,7 +46,7 @@ class Orb(_SolidBase):
     def __repr__(self):
         return "Orb : {} {}".format(self.name, self.pRMax)
 
-    def pycsgmesh(self):
+    def pycsgmeshOld(self):
         _log.info("orb.antlr>")
 
         import pyg4ometry.gdml.Units as _Units #TODO move circular import 
@@ -57,4 +57,76 @@ class Orb(_SolidBase):
         _log.info("orb.pycsgmesh>")
         mesh = _CSG.sphere(center=[0,0,0], radius=pRMax,
                            slices=self.nslice, stacks=self.nstack)
+        return mesh
+
+    def pycsgmesh(self):
+
+        _log.info("orb.antlr>")
+
+        import pyg4ometry.gdml.Units as _Units  # TODO move circular import 
+        luval = _Units.unit(self.lunit)
+
+        pRMax = self.evaluateParameter(self.pRMax) * luval
+
+        polygons = []
+
+        _log.info("orb.pycsgmesh>")
+
+
+        dPhi = 2 * _np.pi / self.nslice
+        dTheta = _np.pi / self.nstack
+        
+        for i in range(0, self.nslice, 1):
+
+            i1 = i
+            i2 = i+1
+
+            p1 = dPhi*i1
+            p2 = dPhi*i2
+
+            for j in range(0, self.nstack, 1) :
+                j1 = j
+                j2 = j+1
+
+                t1 = dTheta * j1
+                t2 = dTheta * j2
+
+
+                xRMaxP1T1 = pRMax * _np.sin(t1) * _np.cos(p1)
+                yRMaxP1T1 = pRMax * _np.sin(t1) * _np.sin(p1)
+                zRMaxP1T1 = pRMax * _np.cos(t1)
+
+                xRMaxP2T1 = pRMax * _np.sin(t1) * _np.cos(p2)
+                yRMaxP2T1 = pRMax * _np.sin(t1) * _np.sin(p2)
+                zRMaxP2T1 = pRMax * _np.cos(t1)
+
+                xRMaxP1T2 = pRMax * _np.sin(t2) * _np.cos(p1)
+                yRMaxP1T2 = pRMax * _np.sin(t2) * _np.sin(p1)
+                zRMaxP1T2 = pRMax * _np.cos(t2)
+
+                xRMaxP2T2 = pRMax * _np.sin(t2) * _np.cos(p2)
+                yRMaxP2T2 = pRMax * _np.sin(t2) * _np.sin(p2)
+                zRMaxP2T2 = pRMax * _np.cos(t2)
+
+                if t1 == 0 :                 # if north pole (triangles)
+                    vCurv = []
+                    vCurv.append(_Vertex([xRMaxP1T1, yRMaxP1T1, zRMaxP1T1], None))
+                    vCurv.append(_Vertex([xRMaxP1T2, yRMaxP1T2, zRMaxP1T2], None))
+                    vCurv.append(_Vertex([xRMaxP2T2, yRMaxP2T2, zRMaxP2T2], None))
+                    polygons.append(_Polygon(vCurv))
+                elif t2 == _np.pi :   # if south pole (triangleS)
+                    vCurv = []
+                    vCurv.append(_Vertex([xRMaxP1T1, yRMaxP1T1, zRMaxP1T1], None))
+                    vCurv.append(_Vertex([xRMaxP2T2, yRMaxP2T2, zRMaxP2T2], None))
+                    vCurv.append(_Vertex([xRMaxP2T1, yRMaxP2T1, zRMaxP2T1], None))
+                    polygons.append(_Polygon(vCurv))
+                else :                      # normal curved quad
+                    vCurv = []
+                    vCurv.append(_Vertex([xRMaxP1T1, yRMaxP1T1, zRMaxP1T1], None))
+                    vCurv.append(_Vertex([xRMaxP1T2, yRMaxP1T2, zRMaxP1T2], None))
+                    vCurv.append(_Vertex([xRMaxP2T2, yRMaxP2T2, zRMaxP2T2], None))
+                    vCurv.append(_Vertex([xRMaxP2T1, yRMaxP2T1, zRMaxP2T1], None))
+                    polygons.append(_Polygon(vCurv))
+
+        mesh = _CSG.fromPolygons(polygons)
         return mesh
