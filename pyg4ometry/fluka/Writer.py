641a193e2b1eec512fb42faf7b8887f33cac4c3a
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2019-12-31 16:29:12 +0000

    Start of fluka writer

---
+++ pyg4ometry/fluka/Writer.py
@@ -0,0 +1,14 @@
+class Writer :
+    def __init__(self):
+        pass
+
+    def addDetector(self, flukaRegistry):
+        self.flukaRegistry = flukaRegistry
+
+    def write(self, filename):
+        pass
+
+        # loop over bodies
+
+        # loop over regions
+
