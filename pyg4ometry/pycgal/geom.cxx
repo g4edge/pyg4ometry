a1208704492378784417ff7d813d0b23e6da2d0b
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2020-05-04 10:04:34 +0100

    List and array

--- pyg4ometry/pycgal/geom.cxx
+++ pyg4ometry/pycgal/geom.cxx
@@ -6,6 +6,10 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/numpy.h>
+
+namespace py = pybind11;
 
 class Vector {
 protected : 
@@ -25,6 +29,30 @@ public:
     _y = y;
     _z = z;
   }
+
+  Vector(py::list list)
+  {
+    _x = list[0].cast<double>();
+    _y = list[1].cast<double>();
+    _z = list[2].cast<double>();
+  }
+
+  Vector(py::array_t<double> array) 
+  {
+    py::buffer_info buf = array.request();
+
+    if (buf.ndim != 1)
+      throw std::runtime_error("numpy.ndarray dims must be 1");
+    
+    if (buf.shape[0] != 3) 
+      throw std::runtime_error("numpy.ndarray must be length 3");      
+
+    double* ptr = (double*)buf.ptr;
+    _x = ptr[0];
+    _y = ptr[1];
+    _z = ptr[2];    
+  }
+  
 
   ~Vector() {};
   
@@ -109,19 +137,26 @@ public:
 class Vertex {
 protected:
   Vector _pos;
+  Vector _normal;
 
 public:
-  Vertex(Vector pos) {_pos = pos;};
+  Vertex(Vector pos) {_pos = Vector(pos);};
+  Vertex(Vector pos, Vector normal) {
+    _pos = Vector(pos);
+    _normal = Vector(normal);
+  }
   ~Vertex() {};
   Vector pos() {return _pos;}
-  
+  Vector normal() {return _normal;}
 };
 
-namespace py = pybind11;
 
 PYBIND11_MODULE(geom, m) {
   py::class_<Vector>(m,"Vector")
+    .def(py::init<>())
     .def(py::init<double, double, double>())
+    .def(py::init<py::list>())
+    .def(py::init<py::array_t<double>>())
     .def("x", &Vector::x)
     .def("y", &Vector::y)
     .def("z", &Vector::z)
@@ -142,6 +177,8 @@ PYBIND11_MODULE(geom, m) {
 
   py::class_<Vertex>(m,"Vertex")
     .def(py::init<Vector>())
-    .def("pos",&Vertex::pos);
+    .def(py::init<Vector, Vector>())
+    .def("pos",&Vertex::pos)
+    .def("normal",&Vertex::normal);
 }
 
