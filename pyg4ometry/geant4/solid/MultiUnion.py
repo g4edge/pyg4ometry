6f236b0b787ab0eaaa46c6c4198a4e68cfe872da
swalker92 <zwap082@live.rhul.ac.uk>
2020-06-10 12:05:37 +0100

    fix bug in MultiUnion.mesh method
    It was implemented like a conventional Boolean solid, the first solids has
    no rotation and translation, with all subsequent solids rotated/translated
    with respect to that.  But this doesn't match geant4's behaviour, there is
    no priveleged solid, they all have a rotation and translation.  This is
    further supported by looking in G4MultiUnion.hh, there is simply "AddNode"
    for adding a solid with a rotation/translation, and there is no ctor with a
    first solid.  So this mesh implementation is now the correct one.

--- pyg4ometry/geant4/solid/MultiUnion.py
+++ pyg4ometry/geant4/solid/MultiUnion.py
@@ -7,7 +7,7 @@ import logging as _log
 
 class MultiUnion(_SolidBase):
     """
-    Union between two or more solids.   
+    Union between two or more solids.
 
     :param name: of solid
     :type name: str
@@ -20,14 +20,15 @@ class MultiUnion(_SolidBase):
 
     """
     def __init__(self, name, objects, transformations, registry, addRegistry=True):
-        # circular import 
+        # circular import
         import pyg4ometry.gdml.Defines as _defines
         import pyg4ometry.geant4       as _g4
 
         self.type            = "MultiUnion"
         self.name            = name
         self.objects         = objects
-        self.transformations = [_defines.upgradeToTransformation(t,registry) for t in transformations]
+        self.transformations = [_defines.upgradeToTransformation(t,registry)
+                                for t in transformations]
 
         self.varNames = ["transformations"]
         self.dependents = []
@@ -35,8 +36,8 @@ class MultiUnion(_SolidBase):
         registry.addSolid(self)
         self.registry = registry
 
-        for obj in objects : 
-            obj.dependents.append(self) 
+        for obj in objects :
+            obj.dependents.append(self)
 
     def __repr__(self):
         return 'Multi Union %s' % (self.name)
@@ -45,26 +46,31 @@ class MultiUnion(_SolidBase):
 
         _log.info('MultiUnion.pycsgmesh>')
 
-        
-        # untransformed first solid
-        m1 = self.objects[0].mesh()
+        result = self.objects[0].mesh()
+        tra2 = self.transformations[0]
+        rot = tbxyz2axisangle(tra2[0].eval())
+        tlate = tra2[1].eval()
+        result.rotate(rot[0], -rad2deg(rot[1]))
+        result.translate(tlate)
 
-        for obj,tra2,idx in zip(self.objects[1:],self.transformations[1:],range(1,len(self.objects))) : 
-            
+        for idx, (solid, tra2) in enumerate(zip(self.objects[1:],
+                                                self.transformations[1:]),
+                                            start=1):
+
             # tranformation
             rot = tbxyz2axisangle(tra2[0].eval())
             tlate = tra2[1].eval()
             _log.info('MulUnion.mesh> rot=%s tlate=%s' % (str(rot),str(tlate)))
-            
-            # get meshes 
-            _log.info('union.mesh> mesh %s' % str(idx))
-            m2 = obj.mesh()
 
-            # apply transform to second mesh 
-            m2.rotate(rot[0],-rad2deg(rot[1]))
-            m2.translate(tlate)
+            # get meshes
+            _log.info('union.mesh> mesh %s' % str(idx))
+            mesh = solid.mesh()
+
+            # apply transform to second mesh
+            mesh.rotate(rot[0],-rad2deg(rot[1]))
+            mesh.translate(tlate)
 
             _log.info('MultiUnion.mesh> union')
-            m1 = m1.union(m2)
+            result = result.union(mesh)
 
-        return m1
+        return result
