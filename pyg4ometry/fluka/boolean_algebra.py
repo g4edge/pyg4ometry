a4c90b90d690a65247f66612ee7357c6b82dde2d
swalker92 <zwap082@live.rhul.ac.uk>
2020-06-11 18:37:57 +0100

    first attempt at an implementation for csg pruning

--- pyg4ometry/fluka/boolean_algebra.py
+++ pyg4ometry/fluka/boolean_algebra.py
@@ -2,6 +2,7 @@ import sympy
 
 import antlr4
 
+from . import vector
 from . import region
 from . import reader
 from .RegionExpression import RegionParserVisitor, RegionParser, RegionLexer
@@ -33,7 +34,6 @@ def zoneToDNFZones(zone):
 
     return zones
 
-
 def toAlgebraicExpression(regzone): # region or zone
     s = regzone.flukaFreeString()
     s = s.strip()
@@ -44,3 +44,31 @@ def toAlgebraicExpression(regzone): # region or zone
     bodyNames = set(b.name for b in regzone.bodies())
     namespace = {name: sympy.Symbol(name) for name in bodyNames}
     return sympy.sympify(s, locals=namespace)
+
+def pruneZone(zone, aabb=None):
+    from IPython import embed; embed()
+    mesh0 = zone.intersections[0].body.mesh()
+    aabb0 = vector.AABB.fromMesh(mesh0)
+    result = aabb0
+
+    filtered_intersections = []
+    for body in zone.intersections[1:]:
+        mesh = body.body.mesh(aabb=aabb)
+        if mesh.isNull():
+            from IPython import embed; embed()
+            continue
+        bodyAABB = vector.AABB.fromMesh(mesh)
+        if result.intersects(bodyAABB):
+            filtered_intersections.append(body)
+        else:
+            from IPython import embed; embed()
+
+    filtered_subtractions = []
+    for body in zone.subtractions:
+        mesh = body.body.mesh(aabb=aabb)
+        if mesh.isNull():
+            from IPython import embed; embed()
+        bodyAABB = vector.AABB.fromMesh(mesh)
+        if result.intersects(bodyAABB):
+            filtered_subtractions.append(body)
+    from IPython import embed; embed()
