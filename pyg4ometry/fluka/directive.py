b06007dfa6788bd3c8c93e846558940ed95d20b0
swalker92 <zwap082@live.rhul.ac.uk>
2020-06-27 19:45:37 +0100

    final rototranslation fix.
    The writing was correct but it was incorrect internally.  This is the fix
    that fixes it properly.  Don't reverse the rototranslations, but store the
    rtrans in the same order they were being written out, as we know as they
    were written (and then reloaded) they were correct.

--- pyg4ometry/fluka/directive.py
+++ pyg4ometry/fluka/directive.py
@@ -349,14 +349,14 @@ def rotoTranslationFromTBxyz(name, tbxyz, flukaregistry=None):
     """tbxyz = trait bryan angles in radians"""
     # Reverse it's because different convention in FLUKA (passive vs
     # active angles).
-    tbxyz = Three(reverse(tbxyz))
+    tbxyz = Three(tbxyz)
     result = RecursiveRotoTranslation(name, [])
     # Don't append a RotoTranslation for 0-angle rotations.
     # Note that we are converting from radians to degrees here.
 
-    if tbxyz.z:
-        result.append(RotoTranslation(name, axis="z",
-                                      azimuth=-tbxyz[2]*180/np.pi,
+    if tbxyz.x:
+        result.append(RotoTranslation(name, axis="x",
+                                      azimuth=-tbxyz[0]*180/np.pi,
                                       flukaregistry=flukaregistry))
 
     if tbxyz.y:
@@ -364,9 +364,9 @@ def rotoTranslationFromTBxyz(name, tbxyz, flukaregistry=None):
                                       azimuth=-tbxyz[1]*180/np.pi,
                                       flukaregistry=flukaregistry))
 
-    if tbxyz.x:
-        result.append(RotoTranslation(name, axis="x",
-                                      azimuth=-tbxyz[0]*180/np.pi,
+    if tbxyz.z:
+        result.append(RotoTranslation(name, axis="z",
+                                      azimuth=-tbxyz[2]*180/np.pi,
                                       flukaregistry=flukaregistry))
 
     return result
