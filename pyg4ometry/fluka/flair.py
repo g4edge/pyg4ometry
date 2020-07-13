de6307e26d50ede91709af5a64345fb04e95b8b9
swalker92 <zwap082@live.rhul.ac.uk>
2020-07-13 22:26:21 +0100

    install data files properly with setup.py

--- pyg4ometry/fluka/flair.py
+++ pyg4ometry/fluka/flair.py
@@ -1,40 +1,40 @@
-import os as _os
+import pkg_resources
 
-class Flair :
-    def __init__(self, flukaInputFileName = "fluka.inp", extent = None):
-        self.flukaInputFileName    = flukaInputFileName
-        self.flukaTemplateFileName = _os.path.join(_os.path.dirname(__file__), "flair_template.flair")
+
+class Flair:
+    def __init__(self, flukaInputFileName="fluka.inp", extent=None):
+        self.flukaInputFileName = flukaInputFileName
+        self.flukaTemplateFileName = pkg_resources.resource_filename(
+            __name__, "flair_template.flair"
+        )
+
         self.extent = extent
         self.matrialColours = {}
 
     def write(self, fileName):
         # read template file
-        inputFile  = open(self.flukaTemplateFileName)
-        outputFile = open(fileName,"w")
+        inputFile = open(self.flukaTemplateFileName)
+        outputFile = open(fileName, "w")
 
-        for l in inputFile :
+        for l in inputFile:
 
             # replace file name
-            if l.find("_FLUKA_INPUT_FILENAME_") != -1 :
-                newl = l.replace("_FLUKA_INPUT_FILENAME_",self.flukaInputFileName)
+            if l.find("_FLUKA_INPUT_FILENAME_") != -1:
+                newl = l.replace("_FLUKA_INPUT_FILENAME_", self.flukaInputFileName)
                 outputFile.write(newl)
-            elif l.find("_FLUKA_CUSTOM_MATERIAL_INFORMATION_") != -1 :
-                #Material: PORTLAND
+            elif l.find("_FLUKA_CUSTOM_MATERIAL_INFORMATION_") != -1:
+                # Material: PORTLAND
                 #        alpha: 50
                 #        color: #rrggbb
-                #End
+                # End
                 newl = ""
                 outputFile.write(newl)
-            else :
+            else:
                 outputFile.write(l)
 
         inputFile.close()
         outputFile.close()
 
-    def addMaterialColour(self, materialName, color = (0,0,0,0)):
-
-        if not self.materialColours.hasKey(materialName) :
+    def addMaterialColour(self, materialName, color=(0, 0, 0, 0)):
+        if not self.materialColours.hasKey(materialName):
             self.materialColours[materialName] = color
-
-
-
