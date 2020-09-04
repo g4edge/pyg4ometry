b7576fba37765094d470d0760b78021e7b8a96c4
swalker92 <zwap082@live.rhul.ac.uk>
2020-09-04 09:30:07 +0100

    Revert "add alpha channel ini values (so rgba)"
    This reverts commit ba79285f204b8390b91b776e3c1f8b15a50a11ab.

--- pyg4ometry/visualisation/colour.py
+++ pyg4ometry/visualisation/colour.py
@@ -3,20 +3,14 @@ from collections.abc import MutableMapping
 from random import randint
 import pkg_resources
 
-class RGBAHexFormatError(Exception): pass
 
 def randomColour():
     """Return random RGB tuple"""
     return (randint(0, 255), randint(0, 255), randint(0, 255))
 
-def hexRGBAToRGBAQuad(value):
-    """Read a RGBA hex quadtruplet of numbers (e.g. 91BBFFAA) and convert to
-    fractions"""
-    hexes = [value[:2], value[2:4], value[4:6], value[6:]]
-    try:
-        return [int(h, 16) / 255 for h in hexes]
-    except ValueError:
-        raise RGBAHexFormatError(value)
+def hexRGBToRGBTriplet(value):
+    hexes = [value[:2], value[2:4], value[4:]]
+    return [int(h, 16) / 255 for h in hexes]
 
 class ColourMap(MutableMapping):
     def __init__(self, *args, **kwargs):
