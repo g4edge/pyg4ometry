83fb18c16f8bad5d2fa04c15c3be7309c14318f1
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-05-20 16:53:28 +0200

    Fix dummy package code

--- src/pkgplaceholder/__init__.py
+++ src/pkgplaceholder/__init__.py
@@ -1,6 +1,6 @@
 from pkgplaceholder._version import version as __version__
 
-from pkgplaceholder.code import hello_world
+from pkgplaceholder.core import hello_world
 
 __all__ = [
     '__version__',
