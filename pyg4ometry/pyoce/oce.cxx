a95c974a2f3d39ec45e16a07282fda26e99b4a71
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-06-29 10:28:23 +0100

    WIP : Load step file

--- pyg4ometry/pyoce/oce.cxx
+++ pyg4ometry/pyoce/oce.cxx
@@ -1,6 +1,25 @@
 #include "oce.h"
 
-StepFile::StepFile() {}
+#include <TDocStd_Document.hxx>
+#include <NCollection_Vector.hxx>
+#include <XCAFApp_Application.hxx>
+#include <STEPCAFControl_Reader.hxx>
+
+StepFile::StepFile() {
+  Handle(TDocStd_Document) hDoc;
+  Handle(XCAFApp_Application) hApp = XCAFApp_Application::GetApplication();
+
+  hApp->NewDocument(TCollection_ExtendedString("MDTV-CAF"), hDoc);
+
+  STEPCAFControl_Reader aReader;
+  aReader.SetColorMode(true);
+  aReader.SetNameMode(true);
+  aReader.SetLayerMode(true);
+
+  aReader.ReadFile("test.step");
+
+  aReader.Transfer(hDoc);
+}
 
 StepFile::~StepFile() {}
 
--- pyg4ometry/pyoce/oce.h
+++ pyg4ometry/pyoce/oce.h
@@ -8,3 +8,5 @@ class StepFile {
   ~StepFile();  
 };
 
+
+
