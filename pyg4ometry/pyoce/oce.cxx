f1ea69e8fd9edeccd9ee961a8c21f56a7d95440e
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-06-29 19:54:47 +0100

    Rearranged with better members

--- pyg4ometry/pyoce/oce.cxx
+++ pyg4ometry/pyoce/oce.cxx
@@ -1,33 +1,39 @@
 #include "oce.h"
 
-#include <TDocStd_Document.hxx>
 #include <NCollection_Vector.hxx>
-#include <XCAFApp_Application.hxx>
 #include <STEPCAFControl_Reader.hxx>
 
 StepFile::StepFile() {
-  Handle(TDocStd_Document) hDoc;
-  Handle(XCAFApp_Application) hApp = XCAFApp_Application::GetApplication();
 
+  hApp = XCAFApp_Application::GetApplication();
   hApp->NewDocument(TCollection_ExtendedString("MDTV-CAF"), hDoc);
+}
 
+StepFile::~StepFile() {}
+
+void StepFile::loadFile(std::string fileName) {
   STEPCAFControl_Reader aReader;
   aReader.SetColorMode(true);
   aReader.SetNameMode(true);
   aReader.SetLayerMode(true);
 
-  aReader.ReadFile("test.step");
+  aReader.ReadFile(fileName.c_str());
 
   aReader.Transfer(hDoc);
+
+  aShapeTool = XCAFDoc_DocumentTool::ShapeTool(hDoc->Main());
+  aColorTool = XCAFDoc_DocumentTool::ColorTool(hDoc->Main());
 }
 
-StepFile::~StepFile() {}
-
+void StepFile::loadShapes() {
+}
 
 /*********************************************
 PYBIND
 *********************************************/
 PYBIND11_MODULE(oce, m) {
   py::class_<StepFile>(m,"StepFile")
-    .def(py::init<>());
+    .def(py::init<>())
+    .def("loadFile", &StepFile::loadFile);
+
 }
--- pyg4ometry/pyoce/oce.h
+++ pyg4ometry/pyoce/oce.h
@@ -2,10 +2,26 @@
 
 namespace py = pybind11;
 
+#include <string>
+
+#include <TDocStd_Document.hxx>
+#include <XCAFApp_Application.hxx>
+#include <XCAFDoc_DocumentTool.hxx>
+#include <XCAFDoc_ShapeTool.hxx>
+#include <XCAFDoc_ColorTool.hxx>
+
 class StepFile {
  public :
   StepFile();
-  ~StepFile();  
+  ~StepFile();
+  void loadFile(std::string fileName);
+
+ protected :
+  Handle(TDocStd_Document)    hDoc;
+  Handle(XCAFApp_Application) hApp;
+
+  Handle(XCAFDoc_ShapeTool) aShapeTool;
+  Handle(XCAFDoc_ColorTool) aColorTool;
 };
 
 
