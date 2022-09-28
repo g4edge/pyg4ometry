6433eb318c1ddb771a40e2a6881b2ab06657bcae
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-09-28 10:32:21 +0100

    Remove freecad

--- src/pyg4ometry/__init__.py
+++ src/pyg4ometry/__init__.py
@@ -3,23 +3,14 @@ from . import compare
 from . import convert
 from . import exceptions
 from . import fluka
-from . import freecad
 from . import gdml
 from . import io
 from . import geant4
 from . import pycgal
-
-# this will also cause a ModuleNotFoundError exception
 try :
     from . import pyoce
 except ImportError:
     print("Failed to import open cascade")
-#try:
-#    from . import gui
-#except ImportError:
-#    import warnings
-#    warnings.warn("Failed to import pyg4ometry.gui subpackage.")
-#    del warnings
 from . import stl
 from . import transformation
 from . import visualisation
