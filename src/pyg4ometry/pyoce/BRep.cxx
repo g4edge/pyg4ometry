7c1be8da68c4b45d5dc000274f0720b3e007360b
sboogert <stewart.boogert@rhul.ac.uk>
2022-10-02 17:33:22 +0100

    Fixed old OCE version issues

--- src/pyg4ometry/pyoce/BRep.cxx
+++ src/pyg4ometry/pyoce/BRep.cxx
@@ -7,6 +7,7 @@ namespace py = pybind11;
 #include <TopoDS_Face.hxx>
 #include <TopoDS_Edge.hxx>
 #include <TopLoc_Location.hxx>
+#include <Poly_MeshPurpose.hxx>
 
 /*********************************************
 PYBIND
@@ -21,5 +22,9 @@ PYBIND11_MODULE(BRep, m) {
             auto ret = BRep_Tool::Curve(E,L,First,Last);
             return py::make_tuple(ret,L,First,Last);
         })
+#if OCC_VERSION_MAJOR == 7 && OCC_VERSION_MINOR == 6
     .def_static("Triangulation",&BRep_Tool::Triangulation);
+#else
+    .def_static("Triangulation",[](TopoDS_Face &fac, TopLoc_Location &loc, Poly_MeshPurpose) {BRep_Tool::Triangulation(fac,loc,Poly_MeshPurpose_NONE);});
+#endif
 }
