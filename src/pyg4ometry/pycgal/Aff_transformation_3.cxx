d96a4deade53243db7cfa65aca9877dd3e778162
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2022-11-10 23:12:33 +0000

    Complete affine transform, Added bbox

--- src/pyg4ometry/pycgal/Aff_transformation_3.cxx
+++ src/pyg4ometry/pycgal/Aff_transformation_3.cxx
@@ -21,17 +21,86 @@ typedef CGAL::Aff_transformation_3<Kernel_EPECK>              Aff_transformation
 PYBIND11_MODULE(Aff_transformation_3, m) {
 
   py::class_<Aff_transformation_3_EPICK>(m,"Aff_transformation_3_EPICK")
+    /* Creation */
     .def(py::init<const CGAL::Identity_transformation &>())
     .def(py::init<const CGAL::Translation&, const Vector_3_EPICK &>())
-    .def(py::init<double, double, double,
-                  double, double, double,
-                  double, double, double, double>());
+    .def(py::init<const CGAL::Scaling&, const Kernel_EPICK::RT &, const Kernel_EPICK::RT &>())
+    .def(py::init<const CGAL::Scaling&, const double &, const double &>())
+    .def(py::init<const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT,
+                  const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT,
+                  const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT>())
+    .def(py::init<const double, const double, const double, const double,
+                  const double, const double, const double, const double,
+                  const double, const double, const double, const double, const double>())
+    .def(py::init<const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT,
+                  const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT,
+                  const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT, const Kernel_EPICK::RT>())
+    .def(py::init<const double, const double, const double,
+                  const double, const double, const double,
+                  const double, const double, const double, const double>())
+
+    /* Operations */
+    .def("transform",[](const Aff_transformation_3_EPICK &t, const Kernel_EPICK::Point_3 &p) {return t.transform(p);})
+    .def("transform",[](const Aff_transformation_3_EPICK &t, const Kernel_EPICK::Vector_3 &v) {return t.transform(v);})
+    .def("transform",[](const Aff_transformation_3_EPICK &t, const Kernel_EPICK::Direction_3 &d) {return t.transform(d);})
+    .def("transform",[](const Aff_transformation_3_EPICK &t, const Kernel_EPICK::Plane_3 &p) {return t.transform(p);})
+    .def("__call__",[](const Aff_transformation_3_EPICK &t, const Kernel_EPICK::Point_3 &p) {return t.transform(p);})
+    .def("__call__",[](const Aff_transformation_3_EPICK &t, const Kernel_EPICK::Vector_3 &v) {return t.transform(v);})
+    .def("__call__",[](const Aff_transformation_3_EPICK &t, const Kernel_EPICK::Direction_3 &d) {return t.transform(d);})
+    .def("__call__",[](const Aff_transformation_3_EPICK &t, const Kernel_EPICK::Plane_3 &p) {return t.transform(p);})
+    .def("__mul__",[](const Aff_transformation_3_EPICK &t1, const Aff_transformation_3_EPICK &t2) {return t1*t2;})
+    .def("inverse",&Aff_transformation_3_EPICK::inverse)
+    .def("__eq__",[](const Aff_transformation_3_EPICK &t1, const Aff_transformation_3_EPICK &t2) {return t1 == t2;})
+    .def("is_even",&Aff_transformation_3_EPICK::is_even)
+    .def("is_odd",&Aff_transformation_3_EPICK::is_odd)
+    .def("is_scaling",&Aff_transformation_3_EPICK::is_scaling)
+    .def("is_translation",&Aff_transformation_3_EPICK::is_translation)
+
+    /* Matrix Entry Access */
+    .def("cartesian",&Aff_transformation_3_EPICK::cartesian)
+    .def("m",&Aff_transformation_3_EPICK::m)
+    .def("m",&Aff_transformation_3_EPICK::homogeneous )
+    .def("hm",&Aff_transformation_3_EPICK::hm);
 
   py::class_<Aff_transformation_3_EPECK>(m,"Aff_transformation_3_EPECK")
+    /* Creation */
     .def(py::init<const CGAL::Identity_transformation &>())
     .def(py::init<const CGAL::Translation&, const Vector_3_EPECK &>())
-    .def(py::init<double, double, double,
-                  double, double, double,
-                  double, double, double, double>());
+    .def(py::init<const CGAL::Scaling&, const Kernel_EPECK::RT &, const Kernel_EPECK::RT &>())
+    .def(py::init<const CGAL::Scaling&, const double &, const double &>())
+    .def(py::init<const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT,
+                  const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT,
+                  const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT>())
+    .def(py::init<const double, const double, const double, const double,
+                  const double, const double, const double, const double,
+                  const double, const double, const double, const double, const double>())
+    .def(py::init<const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT,
+                  const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT,
+                  const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT, const Kernel_EPECK::RT>())
+    .def(py::init<const double, const double, const double,
+                  const double, const double, const double,
+                  const double, const double, const double, const double>())
 
+    /* Operations */
+    .def("transform",[](const Aff_transformation_3_EPECK &t, const Kernel_EPECK::Point_3 &p) {return t.transform(p);})
+    .def("transform",[](const Aff_transformation_3_EPECK &t, const Kernel_EPECK::Vector_3 &v) {return t.transform(v);})
+    .def("transform",[](const Aff_transformation_3_EPECK &t, const Kernel_EPECK::Direction_3 &d) {return t.transform(d);})
+    .def("transform",[](const Aff_transformation_3_EPECK &t, const Kernel_EPECK::Plane_3 &p) {return t.transform(p);})
+    .def("__call__",[](const Aff_transformation_3_EPECK &t, const Kernel_EPECK::Point_3 &p) {return t.transform(p);})
+    .def("__call__",[](const Aff_transformation_3_EPECK &t, const Kernel_EPECK::Vector_3 &v) {return t.transform(v);})
+    .def("__call__",[](const Aff_transformation_3_EPECK &t, const Kernel_EPECK::Direction_3 &d) {return t.transform(d);})
+    .def("__call__",[](const Aff_transformation_3_EPECK &t, const Kernel_EPECK::Plane_3 &p) {return t.transform(p);})
+    .def("__mul__",[](const Aff_transformation_3_EPECK &t1, const Aff_transformation_3_EPECK &t2) {return t1*t2;})
+    .def("inverse",&Aff_transformation_3_EPECK::inverse)
+    .def("__eq__",[](const Aff_transformation_3_EPECK &t1, const Aff_transformation_3_EPECK &t2) {return t1 == t2;})
+    .def("is_even",&Aff_transformation_3_EPECK::is_even)
+    .def("is_odd",&Aff_transformation_3_EPECK::is_odd)
+    .def("is_scaling",&Aff_transformation_3_EPECK::is_scaling)
+    .def("is_translation",&Aff_transformation_3_EPECK::is_translation)
+
+    /* Matrix Entry Access */
+    .def("cartesian",&Aff_transformation_3_EPECK::cartesian)
+    .def("m",&Aff_transformation_3_EPECK::m)
+    .def("m",&Aff_transformation_3_EPECK::homogeneous )
+    .def("hm",&Aff_transformation_3_EPECK::hm);
 }
---
+++ src/pyg4ometry/pycgal/Bbox_3.cxx
@@ -0,0 +1,38 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/stl.h>
+namespace py = pybind11;
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+
+typedef CGAL::Bbox_3                                          Bbox_3;
+
+PYBIND11_MODULE(Bbox_3, m) {
+
+  py::class_<Bbox_3>(m,"Bbox_3")
+    /* Related Functions */
+    //TODO bbox_3, do_overlap
+
+    /* Creation */
+    .def(py::init<>())
+    .def(py::init<double, double,
+                  double, double,
+                  double, double>())
+
+    /* Operations */
+    .def("__eq__",[](const Bbox_3 &b1, const Bbox_3 &b2) {return b1 == b2;})
+    .def("__ne__",[](const Bbox_3 &b1, const Bbox_3 &b2) {return b1 != b2;})
+    .def("dimension",&Bbox_3::dimension)
+    .def("xmin",&Bbox_3::xmin)
+    .def("ymin",&Bbox_3::ymin)
+    .def("zmin",&Bbox_3::zmin)
+    .def("xmax",&Bbox_3::xmax)
+    .def("ymax",&Bbox_3::ymax)
+    .def("zmax",&Bbox_3::zmax)
+    .def("min",&Bbox_3::min)
+    .def("max",&Bbox_3::max)
+    .def("__add__",[](const Bbox_3 &b1, const Bbox_3 &b2) { return b1+b2;})
+    .def("__iadd__",[](Bbox_3 &b1, const Bbox_3 &b2) {b1+=b2; return b1;})
+    .def("dilate",&Bbox_3::dilate);
+}
