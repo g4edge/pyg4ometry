8fc00ae1c52489688d2881e222a9727e64f6e89d
sboogert <stewart.boogert@rhul.ac.uk>
2022-11-18 12:40:46 +0000

    WIP : More CGAL pybind11

--- src/pyg4ometry/pycgal/Iso_cuboid_3.cxx
+++ src/pyg4ometry/pycgal/Iso_cuboid_3.cxx
@@ -15,7 +15,52 @@ typedef CGAL::Exact_predicates_exact_constructions_kernel     Kernel_EPECK;
 typedef Kernel_EPECK::Iso_cuboid_3                            Iso_cuboid_3_EPECK;
 
 PYBIND11_MODULE(Iso_cuboid_3, m) {
-  py::class_<Iso_cuboid_3_EPICK>(m,"Iso_cuboid_3_EPICK");
+  py::class_<Iso_cuboid_3_EPICK>(m,"Iso_cuboid_3_EPICK")
+  /* related functions */
+    .def("__eq__",[](const Iso_cuboid_3_EPICK &c1, const Iso_cuboid_3_EPICK &c2) {return c1 == c2;})
+    .def("__ne__",[](const Iso_cuboid_3_EPICK &c1, const Iso_cuboid_3_EPICK &c2) {return c1 != c2;})
+
+  /* creation */
+    .def(py::init<const Kernel_EPICK::Point_3 &, const Kernel_EPICK::Point_3 &>())
+    .def(py::init<const Kernel_EPICK::Point_3 &, const Kernel_EPICK::Point_3 &, int>())
+    .def(py::init<const Kernel_EPICK::Point_3 &, const Kernel_EPICK::Point_3 &,
+                  const Kernel_EPICK::Point_3 &, const Kernel_EPICK::Point_3 &,
+                  const Kernel_EPICK::Point_3 &, const Kernel_EPICK::Point_3 &>())
+    .def(py::init<const Kernel_EPICK::RT &, const Kernel_EPICK::RT &, const Kernel_EPICK::RT &,
+                  const Kernel_EPICK::RT &, const Kernel_EPICK::RT &, const Kernel_EPICK::RT &,
+                  const Kernel_EPICK::RT &>())
+    .def(py::init<const double &, const double &, const double &,
+                  const double &, const double &, const double &,
+                  const double &>())
+    .def(py::init<const CGAL::Bbox_3 &>())
+
+  /* Operations */
+    .def("__eq__",[](const Iso_cuboid_3_EPICK &c1, const Iso_cuboid_3_EPICK &c2) {return c1 == c2;})
+    .def("__ne__",[](const Iso_cuboid_3_EPICK &c1, const Iso_cuboid_3_EPICK &c2) {return c1 != c2;})
+    .def("vertex",&Iso_cuboid_3_EPICK::vertex)
+    .def("__getitem__",[](const Iso_cuboid_3_EPICK &c1, const int i) {return c1[i];})
+    .def("min",&Iso_cuboid_3_EPICK::min)
+    .def("max",&Iso_cuboid_3_EPICK::max)
+    .def("xmin",&Iso_cuboid_3_EPICK::xmin)
+    .def("ymin",&Iso_cuboid_3_EPICK::ymin)
+    .def("zmin",&Iso_cuboid_3_EPICK::zmin)
+    .def("xmax",&Iso_cuboid_3_EPICK::xmax)
+    .def("ymax",&Iso_cuboid_3_EPICK::ymax)
+    .def("zmax",&Iso_cuboid_3_EPICK::zmax)
+    .def("min_coord",&Iso_cuboid_3_EPICK::min_coord)
+    .def("max_coord",&Iso_cuboid_3_EPICK::max_coord)
+
+  /* predicates */
+    .def("is_degenerate",&Iso_cuboid_3_EPICK::is_degenerate)
+    .def("bounded_side",&Iso_cuboid_3_EPICK::bounded_side)
+    .def("has_on_boundary",&Iso_cuboid_3_EPICK::has_on_boundary)
+    .def("has_on_bounded_side",&Iso_cuboid_3_EPICK::has_on_bounded_side)
+    .def("has_on_unbounded_side",&Iso_cuboid_3_EPICK::has_on_unbounded_side)
+
+  /* Miscellaneous */
+    .def("volume",&Iso_cuboid_3_EPICK::volume)
+    .def("bbox",[](const Iso_cuboid_3_EPICK &c) {return c.bbox();})
+    .def("transform",&Iso_cuboid_3_EPICK::transform);
 
   py::class_<Iso_cuboid_3_EPECK>(m,"Iso_cuboid_3_EPECK")
   /* related functions */
@@ -37,9 +82,30 @@ PYBIND11_MODULE(Iso_cuboid_3, m) {
     .def(py::init<const CGAL::Bbox_3 &>())
 
   /* Operations */
+    .def("__eq__",[](const Iso_cuboid_3_EPECK &c1, const Iso_cuboid_3_EPECK &c2) {return c1 == c2;})
+    .def("__ne__",[](const Iso_cuboid_3_EPECK &c1, const Iso_cuboid_3_EPECK &c2) {return c1 != c2;})
+    .def("vertex",&Iso_cuboid_3_EPECK::vertex)
+    .def("__getitem__",[](const Iso_cuboid_3_EPECK &c1, const int i) {return c1[i];})
+    .def("min",&Iso_cuboid_3_EPECK::min)
+    .def("max",&Iso_cuboid_3_EPECK::max)
+    .def("xmin",&Iso_cuboid_3_EPECK::xmin)
+    .def("ymin",&Iso_cuboid_3_EPECK::ymin)
+    .def("zmin",&Iso_cuboid_3_EPECK::zmin)
+    .def("xmax",&Iso_cuboid_3_EPECK::xmax)
+    .def("ymax",&Iso_cuboid_3_EPECK::ymax)
+    .def("zmax",&Iso_cuboid_3_EPECK::zmax)
+    .def("min_coord",&Iso_cuboid_3_EPECK::min_coord)
+    .def("max_coord",&Iso_cuboid_3_EPECK::max_coord)
 
   /* predicates */
+    .def("is_degenerate",&Iso_cuboid_3_EPECK::is_degenerate)
+    .def("bounded_side",&Iso_cuboid_3_EPECK::bounded_side)
+    .def("has_on_boundary",&Iso_cuboid_3_EPECK::has_on_boundary)
+    .def("has_on_bounded_side",&Iso_cuboid_3_EPECK::has_on_bounded_side)
+    .def("has_on_unbounded_side",&Iso_cuboid_3_EPECK::has_on_unbounded_side)
 
   /* Miscellaneous */
-    .def("bbox",[](const Iso_cuboid_3_EPECK &c) {return c.bbox();});
+    .def("volume",&Iso_cuboid_3_EPECK::volume)
+    .def("bbox",[](const Iso_cuboid_3_EPECK &c) {return c.bbox();})
+    .def("transform",&Iso_cuboid_3_EPECK::transform);
 }
--- src/pyg4ometry/pycgal/__init__.py
+++ src/pyg4ometry/pycgal/__init__.py
@@ -4,16 +4,20 @@ from .Point_2 import Point_2_EPICK as Point_2_EPICK
 from .Point_2 import Point_2_EPECK as Point_2_EPECK
 from .Point_2 import Point_2_EPICK as Partition_traits_2_Point_2_EPICK
 from .Point_2 import Point_2_EPECK as Partition_traits_2_Point_2_EPECK
-from . import Point_3
 from . import Vector_2
+from . import Polygon_2
+from . import Partition_traits_2_Polygon_2
+from . import Polygon_with_holes_2
+from . import Point_3
 from . import Vector_3
 from . import Direction_3
 from . import Triangle_3
 from . import Plane_3
-from . import Polygon_2
-from . import Partition_traits_2_Polygon_2
-from . import Polygon_with_holes_2
 from . import Aff_transformation_3
+from . import Iso_cuboid_3
+from . import Line_3
+from . import Ray_3
+from . import Segment_3
 from . import Polyhedron_3
 from . import Nef_polyhedron_3
 from . import Surface_mesh
