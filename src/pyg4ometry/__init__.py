deaa02e8020fe5a57473572ac4f835ae265d18ae
sboogert <stewart.boogert@rhul.ac.uk>
2023-03-03 17:47:48 +0000

    Move properly to setuptools scm

--- src/pyg4ometry/__init__.py
+++ src/pyg4ometry/__init__.py
@@ -1,3 +1,10 @@
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
+
 from . import config
 from . import compare
 from . import convert
