92c17b4d89b6f40da1291f5e8cc2ae1f9d6942c3
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-04-13 23:00:01 +0200

    re-add libxi

--- .github/bin/install-deps-ubuntu.sh
+++ .github/bin/install-deps-ubuntu.sh
@@ -2,6 +2,6 @@
 
 apt-get -y update
 apt-get -y install libboost-dev libcgal-dev libgmp-dev libmpfr-dev \
-           libocct-* libtbb-dev libvtk9-dev occt-misc
+           libtbb-dev libvtk9-dev libxi-dev libocct-* occt-misc
 
 patch -R /usr/include/opencascade/TDocStd_Application.hxx < .github/patches/TDocStd_Application.patch
