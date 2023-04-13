bb5ffca009f20a814244cc55990a9577ec92c148
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-04-13 20:46:54 +0200

    forgot

--- .github/bin/install-deps-ubuntu.sh
+++ .github/bin/install-deps-ubuntu.sh
@@ -4,4 +4,4 @@ apt-get -y update
 apt-get -y install libboost-dev libcgal-dev libgmp-dev libmpfr-dev \
            libocct-* libtbb-dev libvtk9-dev occt-misc
 
-patch -R /usr/include/opencascade/TDocStd_Application.hxx < docker/patches/TDocStd_Application.patch
+patch -R /usr/include/opencascade/TDocStd_Application.hxx < .github/patches/TDocStd_Application.patch
