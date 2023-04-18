52ed29005c6789c36017b941021504c6f9e2e640
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-04-18 18:27:32 +0200

    Remove OCE header patching on GHA runners

--- .github/bin/install-deps-ubuntu.sh
+++ .github/bin/install-deps-ubuntu.sh
@@ -2,8 +2,4 @@
 
 apt-get -y update
 apt-get -y install libboost-dev libcgal-dev libgmp-dev libmpfr-dev \
-           libtbb-dev libvtk9-dev libxi-dev libocct-* occt-misc
-
-# this is certainly needed for jammy (OCE v7.5) and certainly not needed in v7.7 anymore
-# not sure about v7.6
-patch -R /usr/include/opencascade/TDocStd_Application.hxx < .github/patches/TDocStd_Application.patch
+           libvtk9-dev libxi-dev libocct-*-dev occt-misc
