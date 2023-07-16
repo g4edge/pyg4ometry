aca66d519c42b1c1c779b8652d1ed5c5b6d5d44d
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2023-07-16 17:50:49 +0100

    test analysis code

---
+++ tests/analysis/test_flukaAnalysis.py
@@ -0,0 +1,8 @@
+import pytest
+
+import T001_simple_dump
+
+def test_T001_simple_dump(tmptestdir):
+    T001_simple_dump.Test(
+        vis=False, interactive=False, fluka=True, outputPath=tmptestdir
+    )
