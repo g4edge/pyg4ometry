a0f2b2710e75140a772aee4986c9ea0d1f97e9cf
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2023-07-30 12:43:06 +0100

    pycgal/extruder : (api) Improve list interfaces to pycgal decomposition code
    - Improved doc strings for PolygonProcessing functions
    - List interfaces for Polygon and Polygon_With_Holes
    - Functions of quickly convert polygons to lists and lists to polygons

--- src/pyg4ometry/fluka/extruder.py
+++ src/pyg4ometry/fluka/extruder.py
@@ -3,6 +3,7 @@ import pyg4ometry.geant4.solid
 import pyg4ometry.pycgal.CGAL
 import pyg4ometry.pycgal.pythonHelpers
 
+from ..pycgal import PolygonProcessing
 from ..pycgal import Polygon_2
 from ..pycgal import Polygon_with_holes_2
 from ..pycgal import Point_2
@@ -35,35 +36,16 @@ class Extruder(pyg4ometry.geant4.solid.SolidBase):
     def buildCgalPolygons(self):
         # build a CGAL polygon (without holes for each region)
 
-        for region in self.regions:
-            self.cgalpolys[region] = Polygon_2.Polygon_2_EPECK()
 
-            for point in self.regions[region]:
-                self.cgalpolys[region].push_back(
-                    Point_2.Point_2_EPECK(point[0], point[1])
-                )
-
-        outer_polygon = self.cgalpolys[self.boundary]
-        outer_polygon_hole = (
-            pyg4ometry.pycgal.Polygon_with_holes_2.List_Polygon_with_holes_2_EPECK()
-        )
+        holes = []
 
         for region in self.regions:
             if self.boundary != region:
-                pyg4ometry.pycgal.CGAL.difference(
-                    outer_polygon, self.cgalpolys[region], outer_polygon_hole
-                )
-                outer_polygon = outer_polygon_hole[0]
-                self.decomposed[
-                    region
-                ] = pyg4ometry.pycgal.CGAL.PolygonDecomposition_2_wrapped(
-                    self.cgalpolys[region]
-                )
-        self.decomposed[
-            self.boundary
-        ] = pyg4ometry.pycgal.CGAL.PolygonWithHolesConvexDecomposition_2_wrapped(
-            outer_polygon_hole[0]
-        )
+                self.decomposed[region] = PolygonProcessing.decomposePolygon2d(self.regions[region])
+                holes.append(self.regions[region])
+
+        self.decomposed[self.boundary] = PolygonProcessing.decomposePolygon2dWithHoles(self.regions[self.boundary], holes)
+
 
     def buildGeant4Extrusions(self):
         # normal geant4 extrusions
@@ -89,7 +71,7 @@ class Extruder(pyg4ometry.geant4.solid.SolidBase):
             for pgon, idecomp in zip(pgonList, range(0, len(pgonList))):
                 g4e = pyg4ometry.geant4.solid.ExtrudedSolid(
                     region + "_" + str(idecomp),
-                    pyg4ometry.pycgal.pythonHelpers.polygon_to_numpy(pgon),
+                    _np.array(pgon),
                     [[-self.length / 2, [0, 0], 1], [self.length / 2, [0, 0], 1]],
                     self.registry,
                     lunit="mm",
@@ -114,7 +96,8 @@ class Extruder(pyg4ometry.geant4.solid.SolidBase):
 
         for dr in self.decomposed:
             for p in self.decomposed[dr]:
-                pyg4ometry.pycgal.pythonHelpers.draw_polygon_2(p)
+                p = _np.array(p)
+                _plt.plot(p[:, 0], p[:, 1])
 
     def mesh(self):
         return self.g4_extrusions[self.boundary].mesh()
