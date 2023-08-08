e9c84f8f7d0eb8e2ab40b8a62343bab41b713d3a
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2023-08-08 15:46:26 +0100

    fluka : (feature) Have both converts for a while

--- src/pyg4ometry/convert/__init__.py
+++ src/pyg4ometry/convert/__init__.py
@@ -1,5 +1,6 @@
 from pyg4ometry.convert.fluka2Geant4 import fluka2Geant4
 from pyg4ometry.convert.geant42Fluka import *
+import pyg4ometry.convert.geant42FlukaBake as geant42FlukaBake
 from pyg4ometry.convert.freecad2Fluka import *
 from pyg4ometry.convert.stl2gdml import *
 from pyg4ometry.convert.geant42Geant4 import *
