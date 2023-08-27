52a9fd596a123f6f8ead6d3428a025f0a9ef0dc2
Laurie Nevay <laurie.nevay@cern.ch>
2023-08-27 16:38:43 +0200

    Fix many errors in manual generation.

--- src/pyg4ometry/visualisation/VtkExporter.py
+++ src/pyg4ometry/visualisation/VtkExporter.py
@@ -19,8 +19,7 @@ except (ImportError, ImportWarning):
 class VtkExporter:
     def __init__(self, path="."):
         """
-        Args:
-            path: output repository path
+        :param path: output repository path
         """
         # directory path
         self.path = path
@@ -55,14 +54,13 @@ class VtkExporter:
         df_model and df_color are optional: if they are not given, the Paraview
         model will take the materials colors
 
-        Args:
-            reg: Registry
-            fileName: Name of the Paraview State file (.pvsm)
-            model: Boolean informing whether we work with a "model" GDML or a "simple" GDML
-                True: it will consider that each daughter volume of the GDML world volume will need a .vtm file
-                False: it will create one .vtm file for the whole GDML
-            df_model: (optional) pandas.DataFrame linking the NAME of the element and their TYPE
-            df_color: (optional) pandas.DataFrame linking the TYPE with its specific R G B color
+        :param reg: Registry
+        :param fileName: Name of the Paraview State file (.pvsm)
+        :param model: Boolean informing whether we work with a "model" GDML or a "simple" GDML
+        True: it will consider that each daughter volume of the GDML world volume will need a .vtm file
+        False: it will create one .vtm file for the whole GDML
+        :param df_model: (optional) pandas.DataFrame linking the NAME of the element and their TYPE
+        :param df_color: (optional) pandas.DataFrame linking the TYPE with its specific R G B color
         """
         if _WITH_PARAVIEW:
             self.export_to_VTK(reg, model, df_model, df_color)
@@ -99,13 +97,12 @@ class VtkExporter:
         """
         Method that exports the visible logical volumes of the registry reg into Paraview VTK files (.vtm).
 
-        Args:
-            reg: Registry
-            model: Boolean informing whether we work with a "model" GDML or a "simple" GDML
-                True: it will consider that each daughter volume of the GDML world volume will need a .vtm file
-                False: it will create one .vtm file for the whole GDML
-            df_model: (optional) pandas.DataFrame linking the NAME of the element and their TYPE
-            df_color: (optional) pandas.DataFrame linking the TYPE with its specific R G B color
+        :param reg: Registry
+        :param model: Boolean informing whether we work with a "model" GDML or a "simple" GDML
+        True: it will consider that each daughter volume of the GDML world volume will need a .vtm file
+        False: it will create one .vtm file for the whole GDML
+        :param df_model: (optional) pandas.DataFrame linking the NAME of the element and their TYPE
+        :param df_color: (optional) pandas.DataFrame linking the TYPE with its specific R G B color
         """
         world_volume = reg.getWorldVolume()
 
@@ -123,13 +120,12 @@ class VtkExporter:
         color based on the pandas.DataFrame df_color linking the TYPE of the elements and
         their respective RGB color values.
 
-        Args:
-            df_gdml: pandas.DataFrame that represents the GDML Structure
-            df_model: pandas.DataFrame linking the NAME of the element and their TYPE
-            df_color: pandas.DataFrame linking the TYPE with its specific R G B color
+        :param df_gdml: pandas.DataFrame that represents the GDML Structure
+        :param df_model: pandas.DataFrame linking the NAME of the element and their TYPE
+        :param df_color: pandas.DataFrame linking the TYPE with its specific R G B color
 
         Returns: A dictionary with the keys R, G and B whose item is a dictionary
-            with as keys the logical volumes names and as item the respective RGB value
+        with as keys the logical volumes names and as item the respective RGB value
         """
         import pandas as pd
 
@@ -169,15 +165,14 @@ class VtkExporter:
         Method that receives the logical world volume and calls the necessary methods
         to write the .vtm file(s).
 
-        Args:
-            lv: Logical world volume
-            model: Boolean informing whether we work with a "model" GDML or a "simple" GDML
-                True: it will consider that each daughter volume of the GDML world volume will need a .vtm file
-                False: it will create one .vtm file for the whole GDML
-            color_dico: A dictionary with the keys R, G and B whose item is a dictionary
-                with as keys the logical volumes names and as item the respective RGB value
-            rotation: (optional) numpy.matrix
-            translation: (optional) numpy.array
+        :param lv: Logical world volume
+        :param model: Boolean informing whether we work with a "model" GDML or a "simple" GDML
+        True: it will consider that each daughter volume of the GDML world volume will need a .vtm file
+        False: it will create one .vtm file for the whole GDML
+        :param color_dico: A dictionary with the keys R, G and B whose item is a dictionary
+        with as keys the logical volumes names and as item the respective RGB value
+        :param rotation: (optional) numpy.matrix
+        :param translation: (optional) numpy.array
         """
         if model:
             self.add_logical_volume_recursive(lv, rotation, translation, color_dico)
@@ -207,13 +202,12 @@ class VtkExporter:
         Method that receives a logical volume and add calls addMesh() on its mesh.
         The method is recursive and will be called on all the daughter logical volumes of the logical volume.
 
-        Args:
-            lv: Logical volume
-            rotation: numpy.matrix
-            translation: numpy.array
-            color_dico: A dictionary with the keys R, G and B whose item is a dictionary
-                with as keys the logical volumes names and as item the respective RGB value
-            first_level: Boolean indicating if we are at the first level of recursivity
+        :param lv: Logical volume
+        :param rotation: numpy.matrix
+        :param ranslation: numpy.array
+        :param color_dico: A dictionary with the keys R, G and B whose item is a dictionary
+        with as keys the logical volumes names and as item the respective RGB value
+        :param first_level: Boolean indicating if we are at the first level of recursivity
         """
         for pv in lv.daughterVolumes:
             if first_level:
@@ -263,14 +257,12 @@ class VtkExporter:
         Method that converts the mesh into VtkPolyData (.vtp file)
         and gives it the correct rotation, translation and color.
 
-        Args:
-            solid_name: Name of the logical volume solid
-            mesh: Mesh of the logical volume
-            rotation: numpy.matrix
-            translation: numpy.array
-            visOptions: VisualisationOptions instance
+        :param solid_name: Name of the logical volume solid
+        :param mesh: Mesh of the logical volume
+        :param rotation: numpy.matrix
+        :param translation: numpy.array
+        :param visOptions: VisualisationOptions instance
         """
-
         if solid_name in self.localmeshes:
             vtkPD = self.localmeshes[solid_name]
         else:
@@ -322,10 +314,11 @@ class VtkExporter:
                 self.mbindexdico[self.element_name] += 1
 
     def getMaterialVisOptions(self, name):
-        """Method that "cleans" the logical volume material string.
+        """
+        Method that "cleans" the logical volume material string.
 
-        Args:
-            name: raw name of the logical volume material
+        :param name: raw name of the logical volume material
+        :type name: str
 
         Returns: clean name of the logical volume material
         """
@@ -343,10 +336,11 @@ class VtkExporter:
             return self.materialVisualisationOptions["G4_C"]
 
     def getElementName(self, logicalVolumeName):
-        """Method that "cleans" the logical volume name string.
+        """
+        Method that "cleans" the logical volume name string.
 
-        Args:
-            logicalVolumeName: raw name of the logical volume
+        :param logicalVolumeName: raw name of the logical volume
+        :type logicalVolumeName: str
 
         Returns: clean name of the logical volume
         """
@@ -370,12 +364,15 @@ class VtkExporter:
             )
 
     def countVisibleDaughters(self, lv, element_name, n=0):
-        """Method that counts the number of "visible" daughter logical volumes of the mother logical volume lv.
+        """
+        Method that counts the number of "visible" daughter logical volumes of the mother logical volume lv.
 
-        Args:
-            lv: logical volume
-            element_name: name of the element
-            n: number of "visible" daughter volumes
+        :param lv: logical volume
+        :type lv: pyg4ometry.geant4.LogicalVolume
+        :param element_name: name of the element
+        :type element_name: str
+        :param n: number of "visible" daughter volumes
+        :type n: int
 
         Returns: n
         """
