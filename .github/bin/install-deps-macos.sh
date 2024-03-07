537c32fcc299bc198701761daebfc44c102d75fc
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-03-07 18:30:13 +0000

    github : problem with node symlinks

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -3,4 +3,5 @@
 brew update
 # brew install --overwrite python # github's fault
 brew link --overwrite python
+brew link --overwrite node
 brew install boost cgal gmp mpfr opencascade vtk root numdiff
