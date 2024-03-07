be6d7835581fb9b11fbe13c7e4bae25129d4675b
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-03-07 18:09:22 +0000

    github : problem with python symlinks

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 brew update
-brew install --overwrite python # github's fault
+# brew install --overwrite python # github's fault
+brew link --overwrite python
 brew install boost cgal gmp mpfr opencascade vtk root numdiff
