d95af6a31e499b224fc38fbe2e1e98f1f47a3f3b
Luigi Pertoldi <gipert@pm.me>
2024-03-08 15:17:07 +0100

    Update install-deps-macos.sh

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 brew update
-# brew install --overwrite python # github's fault
-#brew link --overwrite python
-#brew link --overwrite node
+brew install --overwrite python@3.11 # github's fault
 brew install boost cgal gmp mpfr opencascade vtk root numdiff
