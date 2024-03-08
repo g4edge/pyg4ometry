ffeb586c9db8932eceaa8e88909ca8d0782d54c6
Manuel Huber <ManuelHu@users.noreply.github.com>
2024-03-08 17:34:59 +0100

    Update install-deps-macos.sh

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -2,4 +2,9 @@
 
 brew update
 brew install --overwrite python # github's fault
+
+brew unlink python@3.11
+brew install --overwrite python@3.11 python@3.12
+brew link python@3.12
+
 brew install boost cgal gmp mpfr opencascade vtk root numdiff
