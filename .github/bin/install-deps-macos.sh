de032e03bced7021de0f471d9ec28f2d52233e08
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-03-12 10:35:26 +0000

    github : another go at unlink node

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -7,6 +7,8 @@ brew unlink python@3.11
 brew install --overwrite python@3.11 python@3.12
 brew link python@3.12
 
-brew link --overwrite node@18
+brew unlink node@18
+brew install --overwrite node@18
+brew link node@18
 
 brew install boost cgal gmp mpfr opencascade vtk root numdiff
