22fd764ad530aa3edfc714ee9ee6a38676104420
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-03-12 10:17:41 +0000

    github : fix from Manuel and unlink node
    # Conflicts:
    #	.github/bin/install-deps-macos.sh

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 brew update
-brew install --overwrite python@3.11 # github's fault
+brew install --overwrite python # github's fault
+
+brew unlink python@3.11
+brew install --overwrite python@3.11 python@3.12
+brew link python@3.12
+
+brew link --overwrite node@18
+
 brew install boost cgal gmp mpfr opencascade vtk root numdiff
