37c50bde22bb4f87e589fb0beeb155b2d660e7c9
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2024-08-16 18:17:49 +0100

    added mcnp sphere surfaces

--- src/pyg4ometry/mcnp/Surfaces.py
+++ src/pyg4ometry/mcnp/Surfaces.py
@@ -44,3 +44,59 @@ class PZ:
 
     def __repr__(self):
         return f"PZ: {self.D}"
+
+class SO:
+    '''
+    Sphere (centered at origin)
+    '''
+    def __init__(self, R):
+        self.R = R
+
+    def __repr__(self):
+        return f"SO: {self.R}"
+
+class S:
+    '''
+    Sphere (general)
+    '''
+    def __init__(self, x, y, z, R):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.R = R
+
+    def __repr__(self):
+        return f"S: {self.x} {self.y} {self.z} {self.R}"
+
+class SX:
+    '''
+    Sphere (centered on x-axis)
+    '''
+    def __init__(self, x, R):
+        self.x = x
+        self.R = R
+
+    def __repr__(self):
+        return f"SX: {self.x} {self.R}"
+
+class SY:
+    '''
+    Sphere (centered on y-axis)
+    '''
+    def __init__(self, y, R):
+        self.y = y
+        self.R = R
+
+    def __repr__(self):
+        return f"SY: {self.y} {self.R}"
+
+class SZ:
+    '''
+    Sphere (centered on z-axis)
+    '''
+    def __init__(self, z, R):
+        self.z = z
+        self.R = R
+
+    def __repr__(self):
+        return f"SZ: {self.z} {self.R}"
