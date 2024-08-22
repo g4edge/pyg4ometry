dc90c8bbbb6564251b0d55825a8ea8328494581e
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2024-08-22 14:31:42 +0100

    comment style consistency

--- src/pyg4ometry/mcnp/Surfaces.py
+++ src/pyg4ometry/mcnp/Surfaces.py
@@ -46,9 +46,10 @@ class PZ:
         return f"PZ: {self.D}"
 
 class SO:
-    '''
+    """
     Sphere (centered at origin)
-    '''
+    """
+
     def __init__(self, R):
         self.R = R
 
@@ -56,9 +57,10 @@ class SO:
         return f"SO: {self.R}"
 
 class S:
-    '''
+    """
     Sphere (general)
-    '''
+    """
+
     def __init__(self, x, y, z, R):
         self.x = x
         self.y = y
@@ -69,9 +71,10 @@ class S:
         return f"S: {self.x} {self.y} {self.z} {self.R}"
 
 class SX:
-    '''
+    """
     Sphere (centered on x-axis)
-    '''
+    """
+
     def __init__(self, x, R):
         self.x = x
         self.R = R
@@ -80,9 +83,10 @@ class SX:
         return f"SX: {self.x} {self.R}"
 
 class SY:
-    '''
+    """
     Sphere (centered on y-axis)
-    '''
+    """
+
     def __init__(self, y, R):
         self.y = y
         self.R = R
@@ -91,9 +95,10 @@ class SY:
         return f"SY: {self.y} {self.R}"
 
 class SZ:
-    '''
+    """
     Sphere (centered on z-axis)
-    '''
+    """
+
     def __init__(self, z, R):
         self.z = z
         self.R = R
@@ -105,6 +110,7 @@ class C_X:
     """
     Cylinder (parallel to x-axis)
     """
+
     def __init__(self, y, z, R):
         self.y = y
         self.z = z
@@ -117,6 +123,7 @@ class C_Y:
     """
     Cylinder (parallel to y-axis)
     """
+
     def __init__(self, x, z, R):
         self.x = x
         self.z = z
@@ -129,6 +136,7 @@ class C_Z:
     """
     Cylinder (parallel to z-axis)
     """
+
     def __init__(self, x, y, R):
         self.x = x
         self.y = y
@@ -141,6 +149,7 @@ class CX:
     """
     Cylinder (on x-axis)
     """
+
     def __init__(self, R):
         self.R = R
 
@@ -151,6 +160,7 @@ class CY:
     """
     Cylinder (on y-axis)
     """
+
     def __init__(self, R):
         self.R = R
 
@@ -161,6 +171,7 @@ class CZ:
     """
     Cylinder (on z-axis)
     """
+
     def __init__(self, R):
         self.R = R
 
