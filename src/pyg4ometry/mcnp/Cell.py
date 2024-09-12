b1907b0e73c902d6645ea66e754e4e050b814850
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2024-09-12 10:44:10 +0100

    add surface options and boolean operations

--- src/pyg4ometry/mcnp/Cell.py
+++ src/pyg4ometry/mcnp/Cell.py
@@ -1,9 +1,53 @@
 class Cell:
-    def __init__(self, reg=None, cellNumber=None):
-        self.surfaceList = []
+    def __init__(self, surfaces=[], reg=None, cellNumber=None):
+        self.surfaceList = surfaces
         self.cellNumber = cellNumber
         if reg:
             reg.addCell(self)
+            self.reg = reg
 
     def addSurface(self, surface):
+        if type(surface) is str and surface in self.reg.surfaceDict:
+            surface = self.reg.surfaceDict[surface]
         self.surfaceList.append(surface)
+
+    def addSurfaces(self, surfaces):
+        self.surfaceList.extend(surfaces)
+
+    def addMacrobody(self, macrobody):
+        self.addSurface(macrobody)
+
+    def addMacrobodies(self, macrobody):
+        self.addSurfaces(macrobody)
+
+
+class Intersection:
+    """
+    mcnp : blank space between two surface numbers
+    pyg4 : asterisk
+    """
+
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Union:
+    """
+    mcnp : colon
+    pyg4 : plus
+    """
+
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Complement:
+    """
+    mcnp : hash
+    pyg4 : hyphen
+    """
+
+    def __init__(self, item):
+        self.item = item
