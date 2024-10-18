a87f072a87326875ff233e14f7add7c510766f43
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-10-18 20:27:51 +0100

    vis: working with LV/PV and placements

--- src/pyg4ometry/visualisation/UsdViewer.py
+++ src/pyg4ometry/visualisation/UsdViewer.py
@@ -1,3 +1,4 @@
+import pyg4ometry as _pyg4
 from .ViewerHierarchyBase import ViewerHierarchyBase as _ViewerHierarchyBase
 
 from pathlib import Path
@@ -20,17 +21,21 @@ class UsdViewer(_ViewerHierarchyBase):
         if not LV:
             LV = self.worldLV
 
-        print(LV.name)
+        print("LV name", LV.name)
 
         if not motherPrim:
             primName = "/" + LV.name
-            meshPrim = self.stage.DefinePrim(primName, "Mesh")
-
+            lvPrim = self.stage.DefinePrim(primName, "Xform")
         else:
             primName = LV.name
-            meshPrim = self.stage.DefinePrim(motherPrim.GetPath().AppendPath(primName), "Mesh")
+            lvPrim = self.stage.DefinePrim(motherPrim.GetPath().AppendPath(primName), "Xform")
 
-        # add data to mesh
+        print("Xform Prim path", lvPrim.GetPath())
+
+        # add mesh prim
+        meshPrim = self.stage.DefinePrim(lvPrim.GetPath().AppendPath(LV.name + "_mesh"), "Mesh")
+        print("Mesh Prim path", meshPrim.GetPath())
+
         m = LV.mesh.localmesh.toVerticesAndPolygons()
         meshPrim.GetAttribute("points").Set(m[0])
         meshPrim.GetAttribute("faceVertexCounts").Set([len(vl) for vl in m[1]])
@@ -39,7 +44,7 @@ class UsdViewer(_ViewerHierarchyBase):
         meshPrim.GetAttribute("faceVertexIndices").Set(a)
 
         for daughter in LV.daughterVolumes:
-            daughterPrim = self.traverseHierarchy(daughter.logicalVolume, motherPrim=meshPrim)
+            daughterPrim = self.traverseHierarchy(daughter.logicalVolume, motherPrim=lvPrim)
 
             # daughter pos
             pos = daughter.position.eval()
@@ -54,7 +59,61 @@ class UsdViewer(_ViewerHierarchyBase):
             # Rotate
             xform.AddRotateXYZOp().Set(Gf.Vec3d(*rot))
 
-        return meshPrim
+        return lvPrim
+
+    def traverseHierarchy2(self, volume=None, motherPrim=None):
+
+        if not volume:
+            volume = self.worldLV
+
+        print("volume name>", volume.name, type(volume))
+
+        # if volume is a logical/physical
+        if not motherPrim:
+            prim = self.stage.DefinePrim("/" + volume.name, "Xform")
+        else:
+            prim = self.stage.DefinePrim(motherPrim.GetPath().AppendPath(volume.name), "Xform")
+
+        if type(volume) is _pyg4.geant4.LogicalVolume:
+
+            print("logical")
+
+            # add mesh prim
+            meshPrim = self.stage.DefinePrim(
+                prim.GetPath().AppendPath(volume.name + "_mesh"), "Mesh"
+            )
+            print("volume mesh>", meshPrim.GetPath())
+
+            m = volume.mesh.localmesh.toVerticesAndPolygons()
+            meshPrim.GetAttribute("points").Set(m[0])
+            meshPrim.GetAttribute("faceVertexCounts").Set([len(vl) for vl in m[1]])
+            a = _np.array(m[1])
+            a.reshape(a.shape[0] * a.shape[1])
+            meshPrim.GetAttribute("faceVertexIndices").Set(a)
+
+            for daughter in volume.daughterVolumes:
+                daughterPrim = self.traverseHierarchy2(daughter, motherPrim=prim)
+
+                # daughter pos
+                if daughter.type == "placement":
+                    pos = _np.array(daughter.position.eval())
+                    # daughter rot
+                    rot = -_np.array(daughter.rotation.eval()) * 180 / _np.pi
+
+                    # Transformation
+                    xform = UsdGeom.Xformable(daughterPrim)
+                    # Translation
+                    xform.AddTranslateOp().Set(Gf.Vec3d(*pos))
+                    # Rotate
+                    xform.AddRotateZYXOp().Set(Gf.Vec3d(*rot))
+
+        elif type(volume) is _pyg4.geant4.PhysicalVolume:
+            print("physical")
+            self.traverseHierarchy2(volume.logicalVolume, motherPrim=prim)
+        else:
+            print("other")
+
+        return prim
 
     def save(self):
         self.stage.Save()
