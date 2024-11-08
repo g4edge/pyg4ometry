0fe8bafc5d34e9b65da056fbd5ac54c584983b0f
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-11-08 00:15:25 +0000

    convert: update reference test data (MultiUnion)

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,5 +35,5 @@ def pytest_sessionfinish(session, exitstatus):
 @pytest.fixture(scope="session")
 def testdata():
     g4data = G4EdgeTestData()
-    g4data.checkout("2c25b5c")
+    g4data.checkout("8bc13ee")
     return g4data
