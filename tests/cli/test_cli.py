a88712d2c360b0209237ae61f6e0d9e21095c788
Laurie Nevay <laurie.nevay@cern.ch>
2025-01-04 14:12:42 +0100

    Completed cli testing.

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -16,19 +16,32 @@ def _pj(filename):
 def test_cli_testing():
     # without the testing=True, the cli should call sys.exit() which
     # we should be able to catch here. Effectively, testing the testing.
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
+    with pytest.raises(SystemExit) as ex:
         _cli.main(["--help"])
-        assert pytest_wrapped_e.type == SystemExit
-        assert pytest_wrapped_e.value.code == 42
+    assert ex.type == SystemExit
+    assert ex.value.code == 0
 
 
-def test_cli_help():
+def test_cli_help_short():
+    with pytest.raises(OptParseError) as ex:
+        _cli.main(["--h"], testing=True)
+    assert ex.type is OptParseError
+
+
+def test_cli_help_long():
     with pytest.raises(OptParseError) as ex:
         _cli.main(["--help"], testing=True)
     assert ex.type is OptParseError
 
 
 # these are done in the order of the definitions added to the parser object in the main function
+
+def test_cli_no_input():
+    with pytest.raises(SystemExit) as ex:
+        _cli.main([], testing=True)
+    assert ex.type == SystemExit
+    assert ex.value.code == 1
+
 
 def test_cli_analysis_short(testdata):
     _cli.main(["-i", testdata["gdml/001_box.gdml"], "-a"], testing=True)
@@ -79,14 +92,14 @@ def test_cli_append_long(testdata):
 
 
 def test_cli_feature_extract_short(testdata):
-    # TODO - change once implememted
+    # TODO - change once implemented
     with pytest.raises(NotImplementedError) as ex:
         _cli.main(["-i", testdata["gdml/001_box.gdml"], "-f", "0"], testing=True)
     assert ex.type is NotImplementedError
 
 
 def test_cli_feature_extract_long(testdata):
-    # TODO - change once implememted
+    # TODO - change once implemented
     with pytest.raises(NotImplementedError) as ex:
         _cli.main(["-i", testdata["gdml/001_box.gdml"], "--feature", "0"], testing=True)
     assert ex.type is NotImplementedError
@@ -108,13 +121,6 @@ def test_cli_load_stl(testdata):
 def test_cli_load_stp(testdata):
     _cli.main(["-i", testdata["step/1_BasicSolids_Bodies.step"]], testing=True)
 
-def test_cli_view_short(testdata):
-    _cli.main(["-i", testdata["gdml/001_box.gdml"], "-v"], testing=True)
-
-
-def test_cli_view_long(testdata):
-    _cli.main(["-i", testdata["gdml/001_box.gdml"], "--view"], testing=True)
-
 
 def test_cli_info_short(testdata):
     _cli.main(["-i", testdata["gdml/001_box.gdml"], "-I", "reg"], testing=True)
@@ -133,22 +139,163 @@ def test_cli_info_long_instances(testdata):
 
 
 def test_cli_logical_short(testdata):
-    _cli.main(["-i", testdata["gdml/CompoundExamples/bdsim/vkickers.gdml"], "-l", "m1_centre_container_lv0x7fc499f3bce0"], testing=True)
+    _cli.main(["-i", testdata["gdml/CompoundExamples/bdsim/vkickers.gdml"],
+               "-l", "m1_centre_container_lv0x7fc499f3bce0"], testing=True)
 
 
 def test_cli_logical_long(testdata):
-    _cli.main(["-i", testdata["gdml/CompoundExamples/bdsim/vkickers.gdml"], "--logical", "m1_centre_container_lv0x7fc499f3bce0"], testing=True)
+    _cli.main(["-i", testdata["gdml/CompoundExamples/bdsim/vkickers.gdml"],
+               "--logical", "m1_centre_container_lv0x7fc499f3bce0"], testing=True)
 
 
 def test_cli_materials_short(testdata):
-    # TODO - change once implememted
+    # TODO - change once implemented
     with pytest.raises(NotImplementedError) as ex:
         _cli.main(["-i", testdata["gdml/001_box.gdml"], "-m", "Fe"], testing=True)
     assert ex.type is NotImplementedError
 
 
 def test_cli_materials_long(testdata):
-    # TODO - change once implememted
+    # TODO - change once implemented
     with pytest.raises(NotImplementedError) as ex:
         _cli.main(["-i", testdata["gdml/001_box.gdml"], "--material", "Fe"], testing=True)
     assert ex.type is NotImplementedError
+
+
+def test_cli_nullmesh_exception_short(testdata):
+    _cli.main(["-i", testdata["gdml/CompoundExamples/bdsim/vkickers.gdml"], "-n"], testing=True)
+
+
+def test_cli_nullmesh_exception_long(testdata):
+    _cli.main(["-i", testdata["gdml/CompoundExamples/bdsim/vkickers.gdml"], "--nullmesh"], testing=True)
+
+
+def test_cli_output_short_gl(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "-o", "o.gl"], testing=True)
+
+
+def test_cli_output_short_html(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "-o", "o.html"], testing=True)
+
+
+def test_cli_output_short_gdml(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "-o", "o.gdml"], testing=True)
+
+
+def test_cli_output_long_gdml(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "--output", "o.gdml"], testing=True)
+
+
+def test_cli_output_short_inp(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "-o", "o.inp"], testing=True)
+
+
+def test_cli_output_short_usd(testdata):
+    # TODO - change once implemented
+    with pytest.raises(NotImplementedError) as ex:
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "-o", "o.usd"], testing=True)
+    assert ex.type is NotImplementedError
+
+
+def test_cli_output_short_wrong(testdata):
+    with pytest.raises(OSError) as ex:
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "-o", "o.rarara"], testing=True)
+    assert ex.type is OSError
+
+
+def test_cli_planecutter_short(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "-p", "0,0,0,0,1,0", "-P", "box_cut.vtkp"], testing=True)
+
+
+def test_cli_planecutter_long(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "--planeCutter", "0,0,0,0,1,0", "-P", "box_cut.vtkp"], testing=True)
+
+
+def test_cli_planecutter_long2(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "--planeCutter", "0,0,0,0,1,0", "--planeCutterOutput", "box_cut.vtkp"], testing=True)
+
+
+def test_cli_planecutter_short_wrong(testdata):
+    with pytest.raises(ValueError) as ex:
+        # no -P given for output - should complain
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "-p", "0,0,0,0,1,0", "-V"], testing=True)
+    assert ex.type is ValueError
+
+
+def test_cli_rotation_short_gdml(testdata):
+    # TODO - change once implemented
+    with pytest.raises(NotImplementedError) as ex:
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "-r", "0,pi,0", "-V"], testing=True)
+    assert ex.type is NotImplementedError
+
+
+def test_cli_rotation_long_gdml(testdata):
+    # TODO - change once implemented
+    with pytest.raises(NotImplementedError) as ex:
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "--rotation", "0,-pi/2,0", "-V"], testing=True)
+    assert ex.type is NotImplementedError
+
+
+def test_cli_solid_substitution_short_gdml(testdata):
+    # TODO - change once implemented
+    with pytest.raises(NotImplementedError) as ex:
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "-s", "Box,px=10,py=20,pz=30", "-x", "box1Vol"], testing=True)
+    assert ex.type is NotImplementedError
+
+
+def test_cli_solid_substitution_long_gdml(testdata):
+    # TODO - change once implemented
+    with pytest.raises(NotImplementedError) as ex:
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "--solid", "Box,px=10,py=20,pz=30",
+                   "-x", "box1Vol", "-V"], testing=True)
+    assert ex.type is NotImplementedError
+
+
+def test_cli_translation_short_gdml(testdata):
+    # TODO - change once implemented
+    with pytest.raises(NotImplementedError) as ex:
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "-t", "0,2,0", "-V"], testing=True)
+    assert ex.type is NotImplementedError
+
+
+def test_cli_translation_long_gdml(testdata):
+    # TODO - change once implemented
+    with pytest.raises(NotImplementedError) as ex:
+        _cli.main(["-i", testdata["gdml/001_box.gdml"], "--translation", "0,-3,0", "-V"], testing=True)
+    assert ex.type is NotImplementedError
+
+
+def test_cli_gltf_scale_short(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "-S", "1000.0", "-o", "o_scaled.gl"], testing=True)
+
+
+def test_cli_gltf_scale_long(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "--gltfScale", "1000.0", "-o", "o_scaled.gl"], testing=True)
+
+
+def test_cli_view_short(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "-v"], testing=True)
+
+
+def test_cli_view_long(testdata):
+    _cli.main(["-i", testdata["gdml/001_box.gdml"], "--view"], testing=True)
+
+
+def test_cli_citation_short(testdata):
+    with pytest.raises(SystemExit) as ex:
+        _cli.main(["-z"], testing=True)
+    assert ex.type is SystemExit
+    assert ex.value.code == 0
+
+
+def test_cli_citation_long(testdata):
+    with pytest.raises(SystemExit) as ex:
+        _cli.main(["--citation"], testing=True)
+    assert ex.type is SystemExit
+    assert ex.value.code == 0
+
+
+# for debugging in pycharm
+#import g4edgetestdata
+#d = g4edgetestdata.G4EdgeTestData()
+#test_cli_materials_long(d)
